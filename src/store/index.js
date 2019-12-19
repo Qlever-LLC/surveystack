b733bb73513a93d8bfddbaaec3fe98a815d4bc4a
Andreas Rudolf <ar@nexus-computing.ch>
2019-12-19 11:21:54 +0100

    Added store modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import modules from './modules';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
+  modules,
+  state: {},
   actions: {
+    reset({ commit }) {
+      // resets state of all the modules
+      Object.keys(modules).forEach(moduleName => commit(`${moduleName}/RESET`));
+    },
   },
-  modules: {
-  },
+  mutations: {},
 });
