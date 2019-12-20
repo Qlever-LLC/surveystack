11baa1e029d1a2c82e93cba850400aa71c252082
Will Gardiner <gardiner.w@gmail.com>
2019-12-20 05:50:03 -0500

    add arrow-parens rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
+    'arrow-parens': [1, 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
