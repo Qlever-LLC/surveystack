aade09d1a5dc6c1e738f2aec987dbd7175ae1af0
Andreas Rudolf <ar@nexus-computing.ch>
2019-12-20 10:30:12 +0100

    Linted store

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -6,11 +6,11 @@
 const requireModule = require.context('.', false, /\.store\.js$/);
 const modules = {};
 
-requireModule.keys().forEach(filename => {
+requireModule.keys().forEach((filename) => {
   // create the module name from fileName
   // remove the store.js extension and capitalize
   const moduleName = filename.replace(/(\.\/|\.store\.js)/g, '');
-  //.replace(/^\w/, c => c.toUpperCase());
+  // .replace(/^\w/, c => c.toUpperCase());
 
   modules[moduleName] = requireModule(filename).default || requireModule(filename);
 });
