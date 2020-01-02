e296507bb95a039df428df7e970a65f805a6fbd9
Andreas Rudolf <ar@nexus-computing.ch>
2020-01-02 11:19:03 +0100

    Re-added feeback view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <v-app>
     <app-navbar />
-
     <v-content>
+      <app-feedback />
+
       <router-view class="pa-3"></router-view>
     </v-content>
   </v-app>
@@ -10,11 +11,13 @@
 
 <script>
 import appNavbar from '@/components/Navbar.vue';
+import appFeedback from '@/components/Feedback.vue';
 
 export default {
   name: 'App',
   components: {
     appNavbar,
+    appFeedback,
   },
 };
 </script>
