c6dbcd71e391a4b1c03a9a07c2bd2dfa38adafa5
Manuel Di Cerbo <mdc+oursci@serverbox.ch>
2020-01-16 13:05:32 +0100

    working on ui

--- src/views/surveys/collect/drafts/_result_id.vue
+++ src/views/surveys/collect/drafts/_result_id.vue
@@ -1,65 +1,71 @@
 <template>
-  <v-container
-    fluid
-    class="pl-8 pr-8"
-  >
+  <v-container class="pl-8 pr-8">
     <v-row>
-      <div class='title'>
-        <p>{{ mock[0].name }}</p>
+      <div class="title">
+        <div class="inner-title">{{ mock[0].name }} is a long but complicated name right?</div>
+        <div class="subtitle-1 count grey--text text--darken-2">Total<br>{{ mock[0].controls.length + 5254}} Questions</div>
+      </div>
+      <div class="infos grey--text text--darken-2">
+        <div>Question {{ questionIdx + 1 }} <kbd>{{ mock[0].controls[questionIdx].name }}</kbd></div>
       </div>
     </v-row>
     <v-row>
-      <app-text-input
-        v-for="(c,idx) in mock[0].controls"
-        :key="idx"
-        :question="c"
-        v-show="idx == questionIdx"
-      ></app-text-input>
+
+      <component
+        :is="currentControl.type"
+        :question="currentControl"
+      >
+      </component>
+
     </v-row>
     <v-footer
-      absolute
+      fixed
       class="font-weight-medium"
     >
-      <v-col
-        class="text-center"
-        cols="6"
-      >
-        <v-btn
-          @click="prev"
-          class="full"
-          outlined
-          depressed
-          large
-          color="primary"
-        >Preivous</v-btn>
-      </v-col>
+      <v-container class="pl-8 pr-8">
+        <v-row>
+          <v-col
+            class="text-center"
+            cols="6"
+          >
+            <v-btn
+              @click="prev"
+              class="full"
+              outlined
+              depressed
+              large
+              color="primary"
+            >Preivous</v-btn>
+          </v-col>
 
-      <v-col
-        v-if="last"
-        class="text-center"
-        cols="6"
-      >
-        <v-btn
-          @click="submit"
-          class="full"
-          depressed
-          large
-          color="primary"
-        >Submit</v-btn>
-      </v-col>
-      <v-col
-        v-else
-        class="text-center"
-        cols="6"
-      >
-        <v-btn
-          @click="next"
-          class="full"
-          depressed
-          large
-          color="primary"
-        >Next</v-btn>
-      </v-col>
+          <v-col
+            v-if="last"
+            class="text-center"
+            cols="6"
+          >
+            <v-btn
+              @click="submit"
+              class="full"
+              depressed
+              large
+              color="primary"
+            >Submit</v-btn>
+          </v-col>
+          <v-col
+            v-else
+            class="text-center"
+            cols="6"
+          >
+            <v-btn
+              @click="next"
+              class="full"
+              depressed
+              large
+              color="primary"
+            >Next</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
 
     </v-footer>
   </v-container>
@@ -67,16 +73,18 @@
 </template>
 
 <script>
-import appTextInput from '@/components/survey/question_types/TextInput.vue';
+import inputText from '@/components/survey/question_types/TextInput.vue';
+import inputNumeric from '@/components/survey/question_types/NumberInput.vue';
+
 import mock from '../../mock';
 
 export default {
   components: {
-    appTextInput,
+    inputText,
+    inputNumeric,
   },
   data() { return { message: 'hello', mock, questionIdx: 0 }; },
   methods: {
-
     prev() {
       if (this.questionIdx > 0) {
         this.questionIdx -= 1;
@@ -95,6 +103,9 @@ export default {
     last() {
       return this.questionIdx >= mock[0].controls.length - 1;
     },
+    currentControl() {
+      return mock[0].controls[this.questionIdx];
+    },
   },
 };
 </script>
@@ -102,5 +113,24 @@ export default {
 <style>
 .full {
   width: 100%;
+}
+
+.count {
+  flex-grow: 0;
+}
+
+.title {
+  /* align-items: center; */
+  line-height: 1.5rem !important;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  margin: 0.2rem 0;
+}
+
+.inner-title {
+  flex-basis: 0;
+  flex-grow: 1;
 }
 </style>
