e196639bb8420bf1467f914496e2724d4769c93e
Manuel Di Cerbo <mdc+oursci@serverbox.ch>
2020-02-03 15:56:18 +0100

    cleanup

--- src/utils/surveyConfig.js
+++ src/utils/surveyConfig.js
@@ -7,12 +7,28 @@ export const defaultControlOptions = {
 };
 
 export const availableControls = [
-  { name: 'text', label: 'Enter some text', type: 'string' },
-  { name: 'number', label: 'Enter a number', type: 'number' },
   {
-    name: 'group', label: 'My group', type: 'group', children: [],
+    name: 'text',
+    label: 'Enter some text',
+    type: 'string',
   },
   {
-    name: 'location', label: 'Pick Location', type: 'location',
+    name: 'number',
+    label: 'Enter a number',
+    type: 'number',
+  },
+  {
+    name: 'group',
+    label: 'My group',
+    type: 'group',
+    children: [],
+  },
+  {
+    name: 'location',
+    label: 'Pick Location',
+    type: 'location',
+    toString(value) {
+      return `lat: ${value.lat} lon: ${value.lon}`;
+    },
   },
 ];
