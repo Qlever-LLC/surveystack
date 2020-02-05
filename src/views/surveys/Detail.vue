bf87a758cb2c907213333c6a47bec96a50925711
Andreas Rudolf <ar@nexus-computing.ch>
2020-02-05 12:06:15 +0100

    Start submission from intermediate survey page

--- src/views/surveys/Detail.vue
+++ src/views/surveys/Detail.vue
@@ -1,7 +1,61 @@
 <template>
-  <v-container></v-container>
+  <v-container
+    v-if="entity"
+    class="mt-2"
+  >
+    <div class="d-flex justify-end">
+      <v-btn
+        class="mx-2"
+        :to="`/surveys/${entity._id}/edit`"
+      >
+        <v-icon>mdi-pencil</v-icon>
+        <span class="ml-2">Edit</span>
+      </v-btn>
+      <v-btn
+        class="mx-2"
+        :to="`/submissions?survey=${entity._id}`"
+      >
+        <v-icon>mdi-table</v-icon>
+        <span class="ml-2">Results</span>
+      </v-btn>
+
+    </div>
+    <h1>{{entity.name}}</h1>
+    <p>This is a good survey with 17 submissions.</p>
+
+    <div class="mt-4">
+      <v-btn
+        x-large
+        block
+        color="primary"
+        @click="startDraft(entity._id)"
+      >
+        <v-icon>mdi-file-document-box-plus-outline</v-icon>
+        <span class="ml-2">Start Survey</span>
+      </v-btn>
+    </div>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import api from '@/services/api.service';
+
+export default {
+  data() {
+    return {
+      entity: null,
+    };
+  },
+  methods: {
+    startDraft(survey) {
+      this.$store.dispatch('submissions/startDraft', { survey });
+    },
+  },
+  async created() {
+    const { id } = this.$route.params;
+    const { data } = await api.get(`/surveys/${id}`);
+    this.entity = data;
+  },
+
+};
 </script>
