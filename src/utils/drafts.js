c024c10568766c79d997f6309aa230eba79ee481
Andreas Rudolf <ar@nexus-computing.ch>
2020-02-06 09:13:03 +0100

    Working on drafts utils

--- src/utils/drafts.js
+++ src/utils/drafts.js
@@ -1,4 +1,9 @@
+import ObjectID from 'bson-objectid';
+import _ from 'lodash';
+import { unflatten } from 'flat';
 import * as db from '@/store/db';
+import { getSurveyPositions } from './surveys';
+
 
 export const loadResults = () => new Promise((resolve, reject) => {
   db.openDb(() => {
@@ -11,3 +16,46 @@ export const loadResults = () => new Promise((resolve, reject) => {
     });
   });
 });
+
+/**
+ * Creates and returns a new submission based off a specific survey version
+ * @param {Object} survey
+ * @param String version
+ *
+ * @returns {Object} A submission for a specific survey version.
+ */
+export const createSubmissionFromSurvey = (survey, version = 1) => {
+  const clone = _.cloneDeep(survey.versions.find(item => item.version === version));
+  delete clone.dateCreated;
+
+  clone._id = new ObjectID().toString();
+  clone.survey = survey._id;
+  clone.meta = {
+    dateCreated: new Date(),
+    version,
+  };
+
+  // rename "controls" to "data"
+  // https://stackoverflow.com/a/50101979
+  delete Object.assign(clone, { data: clone.controls }).controls;
+
+
+  const positions = getSurveyPositions(survey, version);
+
+  /*
+  const it = processData(instance.data);
+  let res = it.next();
+  const objects = [];
+  while (!res.done) {
+    objects.push(res.value);
+    res = it.next();
+  }
+
+  const c = Object.assign({}, ...objects);
+
+  const u = unflatten(c, { safe: true });
+  return u;
+  */
+
+  return clone;
+};
