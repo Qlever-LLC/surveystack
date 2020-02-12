208965a7ace99246aae9ed38936e7891d4d5373b
Andreas Rudolf <ar@nexus-computing.ch>
2020-02-12 12:58:04 +0100

    Start working on S3 bucket service

--- src/routes/debug.js
+++ src/routes/debug.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+import { uploadToS3 } from '../services/bucket.service';
 
 const router = Router();
 
 router.get('/lucky', async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100 + 1);
   return res.send(`Your lucky number is: ${luckyNumber}`);
+});
+
+router.get('/s3upload', async (req, res) => {
+  const prefix = 'pictures';
+  const filePath = '/home/rudi/Pictures/ar-profile.png';
+  await uploadToS3(prefix, filePath);
+
+  return res.send(`File has been uploaded ${filePath}`);
 });
 
 export default router;
