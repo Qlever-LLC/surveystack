ba0a35ef9015c58a38fc9c0a851a99a6167adc3b
Andreas Rudolf <ar@nexus-computing.ch>
2020-02-17 14:32:44 +0100

    Start debugging csv generation

--- src/routes/debug.js
+++ src/routes/debug.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 import { uploadToS3 } from '../services/bucket.service';
 
+import { db } from '../db';
+import { flatten } from 'flat';
+
+import { ObjectId } from 'mongodb';
+
 const router = Router();
 
 router.get('/lucky', async (req, res) => {
@@ -14,6 +19,28 @@ router.get('/s3upload', async (req, res) => {
   await uploadToS3(prefix, filePath);
 
   return res.send(`File has been uploaded ${filePath}`);
+});
+
+router.get('/submissions', async (req, res) => {
+  const filter = {};
+
+  if (req.query.survey) {
+    filter.survey = new ObjectId(req.query.survey);
+  }
+
+  const submissions = await db
+    .collection('submissions')
+    .find(filter)
+    .toArray();
+
+  const csv = [];
+  submissions.forEach(submission => {
+    submission._id = submission._id.toString();
+    submission.survey = submission.survey.toString();
+    csv.push(flatten(submission));
+  });
+
+  return res.send(csv);
 });
 
 export default router;
