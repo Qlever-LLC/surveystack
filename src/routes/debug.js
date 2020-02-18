c3e0b677b9c965cb34ebda7c0652cd132bf83258
Andreas Rudolf <ar@nexus-computing.ch>
2020-02-18 11:08:08 +0100

    Debugging CSV generation

--- src/routes/debug.js
+++ src/routes/debug.js
@@ -23,6 +23,50 @@ router.get('/s3upload', async (req, res) => {
   return res.send(`File has been uploaded ${filePath}`);
 });
 
+function removeKeys(obj, keys) {
+  for (var prop in obj) {
+    console.log(prop);
+    if (obj.hasOwnProperty(prop)) {
+      switch (typeof obj[prop]) {
+        case 'object':
+          if (keys.indexOf(prop) > -1) {
+            delete obj[prop];
+          } else {
+            removeKeys(obj[prop], keys);
+          }
+          break;
+        default:
+          if (keys.indexOf(prop) > -1) {
+            delete obj[prop];
+          }
+          break;
+      }
+    }
+  }
+}
+
+function removeKeys2(obj, keys) {
+  if (!obj) {
+    return;
+  }
+  for (var prop of Object.keys(obj)) {
+    switch (typeof obj[prop]) {
+      case 'object':
+        if (keys.indexOf(prop) > -1) {
+          delete obj[prop];
+        } else {
+          removeKeys2(obj[prop], keys);
+        }
+        break;
+      default:
+        if (keys.indexOf(prop) > -1) {
+          delete obj[prop];
+        }
+        break;
+    }
+  }
+}
+
 router.get('/submissions', async (req, res) => {
   const filter = {};
 
@@ -39,6 +83,8 @@ router.get('/submissions', async (req, res) => {
   submissions.forEach(submission => {
     submission._id = submission._id.toString();
     submission.survey = submission.survey.toString();
+    removeKeys2(submission.data, ['meta']);
+
     items.push(flatten(submission));
   });
 
