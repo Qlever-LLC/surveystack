ae0f76a6d484066b4807fd5621291105a7c106b2
Andreas Rudolf <ar@nexus-computing.ch>
2020-02-18 11:11:29 +0100

    basic CSV table... with server-side CSV generation!

--- src/views/experiment/sandbox/SandboxAR.vue
+++ src/views/experiment/sandbox/SandboxAR.vue
@@ -1,10 +1,43 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <h1>Andreas' Sandbox</h1>
-    <v-data-table
-      :headers="headers"
-      :items="items"
-    />
+    <v-card class="pt-2">
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        show-select
+        item-key="_id"
+        :search="search"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-row>
+              <v-col>
+                <v-toolbar-title>Simple CSV</v-toolbar-title>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-search"
+                  label="Search"
+                  single-line
+                  autocomplete="off"
+                />
+              </v-col>
+              <v-col :cols="2">
+                <v-switch
+                  v-model="excludeMeta"
+                  label="Hide meta"
+                  class="mt-2"
+                ></v-switch>
+              </v-col>
+            </v-row>
+
+          </v-toolbar>
+        </template>
+      </v-data-table>
+    </v-card>
+
   </v-container>
 </template>
 
@@ -15,8 +48,10 @@ import papa from 'papaparse';
 export default {
   data() {
     return {
+      excludeMeta: true,
       csv: null,
       parsed: null,
+      search: '',
     };
   },
   computed: {
@@ -24,11 +59,12 @@ export default {
       const headers = [];
       if (this.parsed) {
         this.parsed.meta.fields.forEach((header) => {
+          if (this.excludeMeta && header.startsWith('meta')) {
+            return;
+          }
           headers.push({ text: header, value: header });
         });
       }
-      console.log('headers', headers);
-
       return headers;
     },
     items() {
