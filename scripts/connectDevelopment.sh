7389294f9523452051c3032aeb6fd5e159c0b08a
Andreas Rudolf <ar+git@nexus-computing.ch>
2020-03-10 13:36:01 +0100

    Updated connect script

--- scripts/connectDevelopment.sh
+++ scripts/connectDevelopment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PEM_FILE=~/.ssh/oursci-allround.pem
 USER=ubuntu
-SERVER=ec2-52-207-218-148.compute-1.amazonaws.com
+SERVER=ec2-34-230-50-64.compute-1.amazonaws.com
 
 cd $(dirname $0)
 
