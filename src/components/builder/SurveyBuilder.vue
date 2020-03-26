0f31941e2ec5903de4f8fc33a1b00574d3badfdd
Manuel Di Cerbo <mdc+git@nexus-computing.ch>
2020-03-26 22:22:10 +0100

    import / export survey

--- src/components/builder/SurveyBuilder.vue
+++ src/components/builder/SurveyBuilder.vue
@@ -30,6 +30,8 @@
             @saveDraft="saveDraft"
             @delete="$emit('onDelete')"
             @publish="publish"
+            @export-survey="$emit('export-survey')"
+            @import-survey="(file) => $emit('import-survey', file)"
             class="mb-4"
           />
           <graphical-view
@@ -231,6 +233,7 @@ export default {
   props: [
     'survey',
     'editMode',
+    'freshImport',
   ],
   data() {
     return {
@@ -465,6 +468,10 @@ export default {
       return !this.isDraft;
     },
     enableSaveDraft() {
+      if (this.freshImport) {
+        return true;
+      }
+
       if (!this.editMode) { // if survey new
         if (this.initialSurvey.name !== this.survey.name) {
           return true;
