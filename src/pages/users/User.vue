72c02b2a2ec7a1313c526df08ebef3daf5aa4c3e
Andreas Rudolf <ar@nexus-computing.ch>
2020-03-27 13:14:58 +0100

    Show roles in group member list

--- src/pages/users/User.vue
+++ src/pages/users/User.vue
@@ -1,9 +1,30 @@
 <template>
   <v-container>
     <v-card>
-      <v-card-title>{{entity.email}}</v-card-title>
-      <v-card-subtitle>{{entity._id}}</v-card-subtitle>
-      <v-card-text>{{entity.name}}</v-card-text>
+      <v-card-title>{{entity.name}}</v-card-title>
+      <v-card-subtitle>{{entity.email}}</v-card-subtitle>
+      <v-card-text>{{entity._id}}</v-card-text>
+
+    </v-card>
+    <v-card class="mt-3">
+      <v-card-title>Memberships</v-card-title>
+      <v-list>
+        <v-list-item
+          v-for="(m,i) in entity.memberships"
+          :key="`user-${entity._id}-memberships-${i}`"
+          two-line
+          :to="`/g/${m.groupDetail.path?m.groupDetail.path:''}${m.groupDetail.slug}`"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              {{m.groupDetail.name}}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{m.role}}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-card>
   </v-container>
 </template>
@@ -18,12 +39,13 @@ export default {
         _id: '',
         email: '',
         name: '',
+        memberships: [],
       },
     };
   },
   async created() {
     const { id } = this.$route.params;
-    const { data } = await api.get(`/users/${id}`);
+    const { data } = await api.get(`/users/${id}?populate=1`);
     this.entity = { ...this.entity, ...data };
   },
 };
