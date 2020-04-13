6b36adab277d17aaefc9b22b6a5c883fe4e38376
Andreas Rudolf <ar+git@nexus-computing.ch>
2020-04-13 13:25:15 +0200

    Working on deployment scripts

--- scripts/update-development-server.sh
+++ scripts/update-development-server.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+APP_URL=https://dev.our-sci.net
 PEM_FILE=~/.ssh/oursci-allround.pem
-USER=ubuntu
-SERVER=ec2-54-85-87-170.compute-1.amazonaws.com
 
+USER=ubuntu
+SERVER_FALLBACK=ec2-54-85-87-170.compute-1.amazonaws.com
 SERVER_PWA_DIR=/home/ubuntu/repos/development/our-sci-pwa
 
+SERVER=`curl -f -s $APP_URL/api/info/public-hostname`
+
+if [ -z "$SERVER" ]; then
+  echo -n "Enter hostname [$SERVER_FALLBACK]:"
+  read SERVER
+  if [ -z "$SERVER" ]; then
+    SERVER=$SERVER_FALLBACK
+  fi
+else
+  echo "Found public hostname: $SERVER"
+fi
+
+echo "Will use this server: $SERVER"
+
+exit 1
 print_usage() {
   printf "Usage: $0 [ -s ]\n"
   printf "  -s   updates server module only (skip building and uploading client)\n"
@@ -30,12 +46,11 @@ if [ $server_only -eq 0 ]; then
     cd ../our-sci-pwa
     yarn build
     echo "Transfer dist folder to server..."
-    #scp -i "$PEM_FILE" -r dist/ ubuntu@ec2-52-207-218-148.compute-1.amazonaws.com:~/repos/development/our-sci-pwa/
     scp -i "$PEM_FILE" -r dist/ "$USER@$SERVER:$SERVER_PWA_DIR/"
 fi
 
 # run script on server to stop pm2 service, pull from repo, install and restart
 ssh -i "$PEM_FILE" "$USER@$SERVER" << EOF
 cd ~/repos/development/our-sci-server
-bash -i ./scripts/update-server-locally.sh
+bash -i ./scripts/update-development-server-locally.sh
 EOF
--- scripts/update-production-server.sh
+++ scripts/update-production-server.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-PEM_FILE=~/.ssh/oursci-allround.pem
+APP_URL=https://app.surveystack.io
+MODE=production
+PEM_FILE=~/.ssh/surveystack-production.pem
+
 USER=ubuntu
-SERVER_FALLBACK=ec2-54-85-87-170.compute-1.amazonaws.com
+SERVER_FALLBACK=ec2-18-212-28-97.compute-1.amazonaws.com
+SERVER_PWA_DIR=/home/ubuntu/repos/production/our-sci-pwa
 
-SERVER_PWA_DIR=/home/ubuntu/repos/development/our-sci-pwa
-
-echo "Trying to get current server public hostname..."
-SERVER=`curl -f -s https://app.surveystack.io/api/info/public-hostnamee`
+SERVER=`curl -f -s $APP_URL/api/info/public-hostname`
 
 if [ -z "$SERVER" ]; then
-  echo "NOTE: Could not determine server's public hostname!"
-  echo "  Using fallback: $SERVER_FALLBACK"
-  SERVER=$SERVER_FALLBACK
+  echo -n "Enter hostname [$SERVER_FALLBACK]:"
+  read SERVER
+  if [ -z "$SERVER" ]; then
+    SERVER=$SERVER_FALLBACK
+  fi
 else
   echo "Found public hostname: $SERVER"
 fi
@@ -45,12 +48,11 @@ if [ $server_only -eq 0 ]; then
     cd ../our-sci-pwa
     yarn build
     echo "Transfer dist folder to server..."
-    #scp -i "$PEM_FILE" -r dist/ ubuntu@ec2-52-207-218-148.compute-1.amazonaws.com:~/repos/development/our-sci-pwa/
     scp -i "$PEM_FILE" -r dist/ "$USER@$SERVER:$SERVER_PWA_DIR/"
 fi
 
 # run script on server to stop pm2 service, pull from repo, install and restart
 ssh -i "$PEM_FILE" "$USER@$SERVER" << EOF
-cd ~/repos/development/our-sci-server
-bash -i ./scripts/update-server-locally.sh
+cd ~/repos/production/our-sci-server
+bash -i ./scripts/update-production-server-locally.sh
 EOF
