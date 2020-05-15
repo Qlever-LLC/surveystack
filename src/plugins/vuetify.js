d8893cb0711a1616c4d7255fc0dcfcf3355f1127
Andreas Rudolf <ar@nexus-computing.ch>
2020-05-15 16:36:40 +0200

    Added more themeing optoins including a custom 'appbar' property

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,13 +11,14 @@ export default new Vuetify({
     light: true,
     themes: {
       light: {
-        primary: '#f44336',
+        primary: '#03A9F4',
         secondary: '#82AF9C',
         accent: '#FF5722',
         error: '#f44336',
         warning: '#ffc107',
         info: '#82AF9C',
         success: '#8bc34a',
+        appbar: '#ff5555',
       },
     },
   },
