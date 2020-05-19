e141f89fdc6e15992309c0ab5e63ee25b0d7c71f
Andreas Rudolf <ar@nexus-computing.ch>
2020-05-19 12:53:59 +0200

    Continued work on call for submissions

--- src/components/survey/SurveySelector.vue
+++ src/components/survey/SurveySelector.vue
@@ -1,35 +1,33 @@
 <template>
-  <div>
-    <v-dialog
-      v-model="showSearchDialog"
-      max-width="500"
-    >
-      <v-card>
-        <v-card-title>Search surveys</v-card-title>
-        <v-card-text>
-          <v-text-field
-            v-model="q"
-            append-icon="mdi-magnify"
-            @input="(e) => $emit('search', e)"
-          />
-          <v-list>
-            <v-list-item
-              v-for="searchResult in searchResults"
-              :key="searchResult._id"
-              @click="(s) => $emit('selected', s)"
-            >
-              <v-list-item-content>
-                <v-list-item-title>{{searchResult.name}}</v-list-item-title>
-                <v-list-item-subtitle>last modified {{ renderDateFromNow(searchResult.meta.dateModified) }}</v-list-item-subtitle>
+  <v-dialog
+    max-width="500"
+    v-model="dialog"
+  >
+    <v-card>
+      <v-card-title>Search surveys</v-card-title>
+      <v-card-text>
+        <v-text-field
+          v-model="q"
+          append-icon="mdi-magnify"
+          @input="(e) => $emit('search', e)"
+        />
+        <v-list>
+          <v-list-item
+            v-for="searchResult in searchResults"
+            :key="searchResult._id"
+            @click="() => { $emit('selected', searchResult); dialog = false}"
+          >
+            <v-list-item-content>
+              <v-list-item-title>{{searchResult.name}}</v-list-item-title>
+              <v-list-item-subtitle>last modified {{ renderDateFromNow(searchResult.meta.dateModified) }}</v-list-item-subtitle>
 
-              </v-list-item-content>
+            </v-list-item-content>
 
-            </v-list-item>
-          </v-list>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
-  </div>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -41,20 +39,39 @@ export default {
       type: Array,
       required: true,
     },
+    show: {
+      type: Boolean,
+      required: true,
+    },
   },
   data() {
     return {
-      showSearchDialog: true,
       q: '',
     };
+  },
+  computed: {
+    dialog: {
+      get() {
+        return this.show;
+      },
+      set(newValue) {
+        if (!newValue) {
+          this.$emit('hide');
+        }
+      },
+    },
   },
   methods: {
     renderDateFromNow(date) {
       return moment(date).fromNow();
     },
   },
+  mounted() {
+    this.value = true;
+  },
   async created() {
     this.$emit('search', this.q);
+    this.value = true;
   },
 };
 </script>
--- src/pages/call-for-submissions/CallForSubmissions.vue
+++ src/pages/call-for-submissions/CallForSubmissions.vue
@@ -1,21 +1,74 @@
 <template>
   <v-container>
     <h1>Call for Submissions</h1>
-    <app-survey-selector
-      :searchResults="searchResults"
-      @search="searchSurveys"
-    />
-    <v-data-table
-      v-model="selectedMembers"
-      :items="activeMembers"
-      :headers="headers"
-      disable-pagination
-      hide-default-footer
-      show-select
-      item-key="_id"
-    />
 
-    Members selected {{selectedMembers.length}}
+    <app-survey-selector
+      :show="showSelectSurvey"
+      :searchResults="searchResults"
+      @hide="showSelectSurvey = false"
+      @search="searchSurveys"
+      @selected="selectSurvey"
+    />
+    <v-card class="my-2">
+      <v-card-text>
+        <div class="mb-5 d-flex align-center">
+          <v-btn
+            @click="showSelectSurvey = true"
+            color="primary"
+            outlined
+          >Select Survey</v-btn>
+          <span
+            v-if="selectedSurvey"
+            class="mx-2"
+          >{{selectedSurvey.name}}</span>
+          <v-icon
+            v-if="selectedSurvey"
+            @click="selectedSurvey = null"
+          >mdi-close</v-icon>
+        </div>
+
+        <v-text-field
+          v-model="subject"
+          label="Subject"
+          outlined
+        />
+        <v-textarea
+          rows="10"
+          v-model="body"
+          label="Message"
+          outlined
+        />
+        <div class="d-flex justify-end align-center">
+          <span
+            v-if="!submittable"
+            class="mr-2"
+          >Select a survey and at least one member below</span>
+          <v-btn
+            text
+            @click="cancel"
+          >Cancel</v-btn>
+          <v-btn
+            color="primary"
+            :disabled="!submittable"
+          >Send</v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card>
+      <v-card-title>Select members</v-card-title>
+      <v-card-subtitle>{{selectedMembers.length}} selected</v-card-subtitle>
+      <v-card-text>
+        <v-data-table
+          v-model="selectedMembers"
+          :items="activeMembers"
+          :headers="headers"
+          disable-pagination
+          hide-default-footer
+          show-select
+          item-key="_id"
+        />
+      </v-card-text>
+    </v-card>
 
   </v-container>
 </template>
@@ -25,6 +78,15 @@ import appSurveySelector from '@/components/survey/SurveySelector.vue';
 
 import api from '@/services/api.service';
 
+const defaultSubject = 'Request to submit a survey';
+
+const defaultBody = `Hello
+
+Please use the following link to automatically login and start the survey:
+$CFS_MAGIC_LINK$
+
+All the best
+`;
 
 export default {
   components: {
@@ -36,6 +98,10 @@ export default {
       group: null,
       selectedMembers: [],
       searchResults: [],
+      showSelectSurvey: false,
+      selectedSurvey: null,
+      subject: defaultSubject,
+      body: defaultBody,
       headers: [
         { text: 'id', value: '_id' },
         { text: 'name', value: 'user.name' },
@@ -49,10 +115,28 @@ export default {
       const { data: searchResults } = await api.get(`/surveys?projections[]=name&projections[]=meta.dateModified&q=${q}`);
       this.searchResults = searchResults;
     },
+    selectSurvey(survey) {
+      this.selectedSurvey = survey;
+      this.subject = `Request to submit survey '${survey.name}'`;
+      this.showSelectSurvey = false;
+    },
+    cancel() {
+      this.$router.back();
+    },
+  },
+  watch: {
+    selectedSurvey(newVal) {
+      if (!newVal) {
+        this.subject = defaultSubject;
+      }
+    },
   },
   computed: {
     activeMembers() {
       return this.members.filter(member => member.meta.status === 'active');
+    },
+    submittable() {
+      return this.selectedSurvey !== null && this.selectedMembers.length !== 0;
     },
   },
   async created() {
