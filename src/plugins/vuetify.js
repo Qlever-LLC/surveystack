d4a121c8b6a5b2266d7890b16a58c12129c2cdd9
Manuel Di Cerbo <mdc+git@nexus-computing.ch>
2020-05-21 14:02:50 +0200

    update bar color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -12,14 +12,14 @@ export default new Vuetify({
     themes: {
       light: {
         primary: '#03A9F4',
-        secondary: '#f57c73',
+        secondary: '#FF5555',
         accent: '#FF5722',
         error: '#f44336',
         warning: '#ffc107',
         info: '#82AF9C',
         success: '#8bc34a',
         // appbar: '#ffffff',
-        appbar: '#f57c73',
+        appbar: '#FF5555',
       },
     },
   },
