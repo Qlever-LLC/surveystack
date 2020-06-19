4a8c6cf9af249f3ae6ff1f433759987293840fbf
Andreas Rudolf <ar@nexus-computing.ch>
2020-06-19 15:10:36 +0200

    explicitly allow cors credentials

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const frontend = expressStaticGzip('../our-sci-pwa/dist');
 app.use(cookieParser());
 app.use(express.json({ limit: '8mb' }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: '*', credentials: true }));
 
 // auth
 app.use(async (req, res, next) => {
