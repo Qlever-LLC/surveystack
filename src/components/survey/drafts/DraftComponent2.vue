bd0e4d37f7819d5c587783f9b7939b3755dd1a29
Andreas Rudolf <ar+git@nexus-computing.ch>
2020-10-20 16:50:51 +0200

    just for show

--- src/components/survey/drafts/DraftComponent2.vue
+++ src/components/survey/drafts/DraftComponent2.vue
@@ -1,11 +1,26 @@
 <template>
-  <v-container>
-    <h2>Survey</h2>
-    {{survey}}
-    <h2>Submission</h2>
-    {{submission}}
-    <h2>Controls</h2>
-    <v-text-field v-model="name" />
+  <v-container class="mt-12">
+    <v-row class="mt-6">
+      <v-col>
+        <v-textarea
+          :value="submissionStringified"
+          label="submission"
+          rows="30"
+          outlined
+        />
+      </v-col>
+      <v-col>
+        <v-text-field
+          v-model="name"
+          label="data.text_1.value"
+        />
+        <v-text-field
+          v-model="age"
+          label="data.number_2.value"
+        />
+      </v-col>
+    </v-row>
+
   </v-container>
 </template>
 
@@ -24,6 +39,18 @@ export default {
         this.$store.dispatch('draft/setProperty', { key: 'data.text_1.value', value: v });
       },
 
+    },
+    submissionStringified() {
+      return JSON.stringify(this.submission, null, 2);
+    },
+    age: {
+      get() {
+        return this.$store.getters['draft/property']('data.number_2.value');
+      },
+      set(v) {
+        const number = Number(v) || null;
+        this.$store.dispatch('draft/setProperty', { key: 'data.number_2.value', value: number });
+      },
     },
   },
   created() {
