d628ad38d1a382790bb302bff07e1deed21027bb
Andreas Rudolf <ar@nexus-computing.ch>
2020-11-19 18:05:30 +0100

    Working on matrix question

--- src/components/survey/question_types/Matrix.vue
+++ src/components/survey/question_types/Matrix.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    Matrix Type Question
+    <v-data-table
+      :headers="headers"
+      :hide-default-footer="true"
+      :items="rows"
+    >
+      <template v-slot:body="{ items, headers }">
+        <tbody>
+          <tr
+            v-for="(item,idx,k) in items"
+            :key="idx"
+          >
+            <td
+              v-for="(header,key) in headers"
+              :key="key"
+            >
+              <v-text-field
+                :label="header.value"
+                :value="item[header.value]"
+                @input="v => item[header.value] = v"
+                solo
+              />
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-data-table>
+    <div>{{rows}}</div>
+    <div>
+      <v-btn @click="add">ADD ROW</v-btn>
+    </div>
   </div>
 </template>
 
@@ -9,5 +38,21 @@ import baseQuestionComponent from './BaseQuestionComponent';
 
 export default {
   mixins: [baseQuestionComponent],
+  computed: {
+    headers() {
+      const resource = this.resources.find(r => r.id === this.control.options.source);
+      return resource.content.fields.map(f => ({ text: f, value: f }));
+    },
+  },
+  data() {
+    return { rows: [] };
+  },
+  methods: {
+    add() {
+      this.rows.push({
+        field: 'f', operation: 'o', date: 'd', depth: 'd', tilled: 't',
+      });
+    },
+  },
 };
 </script>
