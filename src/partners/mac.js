762c1b0f719bfbdedf1b809009e85bd6127bc323
Manuel Di Cerbo <mdc+git@nexus-computing.ch>
2021-01-06 16:14:50 +0100

    white label million acre challenge

---
+++ src/partners/mac.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'Million Acre Challenge',
+  domain: 'millionacrechallenge',
+  groupSlug: 'million-acre-challenge',
+  logo: '/partners/mac/img/logo.png',
+  themes: {
+    light: {
+      primary: '#6d3a35',
+      secondary: '#82AF9C',
+      accent: '#FF5722',
+      error: '#f44336',
+      warning: '#ffc107',
+      info: '#82AF9C',
+      success: '#8bc34a',
+      appbar: '#FFFFFF',
+    },
+  },
+};
