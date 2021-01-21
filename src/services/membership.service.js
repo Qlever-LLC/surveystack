dee6dbfde00c4e3769dc162bc94fba4d5f06aa1f
Andreas Rudolf <ar@nexus-computing.ch>
2021-01-21 14:12:29 +0100

    Workaround for multiple group memberships issue... accepting an invitation now clears any existing memberships for same group/user

--- src/services/membership.service.js
+++ src/services/membership.service.js
@@ -20,17 +20,27 @@ export const addMembership = async ({ user, group, role }) => {
 };
 
 export const activateMembership = async ({ code, user }) => {
-  const existing = await db.collection(col).findOne({ 'meta.invitationCode': code });
-  if (!existing) {
+  const membership = await db.collection(col).findOne({ 'meta.invitationCode': code });
+  if (!membership) {
     throw boom.notFound(`No invitation found for code: ${code}`);
   }
 
-  if (existing.meta.status !== 'pending') {
+  if (membership.meta.status !== 'pending') {
     throw boom.badRequest(`Code already activated: ${code}`);
   }
 
+  // clear any existing memberships for same user & group
+  const existing = await db.collection(col).find({ user, group: membership.group }).toArray();
+  if (existing.length > 0) {
+    console.log(
+      `User ${user.email} already has ${existing.length} memberships for group ${membership.group}`
+    );
+    console.log('deleting existing membership now...');
+    await db.collection(col).deleteMany({ _id: { $in: existing.map((e) => e._id) } });
+  }
+
   await db.collection(col).findOneAndUpdate(
-    { 'meta.invitationCode': code },
+    { _id: membership._id },
     {
       $set: {
         user: user,
