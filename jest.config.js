ee7e26fc5cd2e7500d4f14ad068a3ac7cf23e834
Will Gardiner <gardiner.w@gmail.com>
2021-05-21 14:02:34 -0400

    add tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   transformIgnorePatterns: ['/node_modules/(?!ol)'],
+  // transformIgnorePatterns: ['/node_modules/'],
   setupFiles: ['jest-canvas-mock'],
+  setupFilesAfterEnv: ['./tests/setup.js'],
+  resetMocks: true,
+  resetModules: true,
 };
--- src/components/survey/question_types/GeoJSON.vue
+++ src/components/survey/question_types/GeoJSON.vue
@@ -1,33 +1,81 @@
 <template>
-    <div class="full-width">
-        <div
-          :id="mapId"
-          :class="{
-            'hide-polygon': !control.options.geoJSON.showPolygon,
-            'hide-line': !control.options.geoJSON.showLine,
-            'hide-point': !control.options.geoJSON.showPoint,
-            'hide-circle': !control.options.geoJSON.showCircle,
-            'hide-move': !control.options.geoJSON.showMove,
-            'hide-modify': !control.options.geoJSON.showModify,
-          }"
-          style="width: 100%; height: 500px"
-        />
-    </div>
+  <div>
+    <app-control-label
+      :value="control.label"
+      :redacted="redacted"
+      :required="required"
+    />
+    <app-control-hint :value="control.hint" />
+    <div
+      :id="mapId"
+      :class="{
+        'map': true,
+        'hide-polygon': !control.options.geoJSON.showPolygon,
+        'hide-line': !control.options.geoJSON.showLine,
+        'hide-point': !control.options.geoJSON.showPoint,
+        'hide-circle': !control.options.geoJSON.showCircle,
+        'hide-move': !control.options.geoJSON.showMove,
+        'hide-modify': !control.options.geoJSON.showModify,
+      }"
+      role="application"
+    />
+    <app-control-more-info :value="control.moreInfo" />
+  </div>
 </template>
 
 <script>
+// import 'ol/ol.css';
 import baseQuestionComponent from './BaseQuestionComponent';
 import createMap from '@/external/instance/instance';
-// function createMap() { return {}; }
+import appControlLabel from '@/components/survey/drafts/ControlLabel.vue';
+import appControlHint from '@/components/survey/drafts/ControlHint.vue';
+import appControlMoreInfo from '@/components/survey/drafts/ControlMoreInfo.vue';
 
+export function addBaseLayer(map) {
+  // Adding an XYZ layer.
+  const xyzOpts = {
+    title: 'mapbox',
+    url: 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoib3Vyc2NpIiwiYSI6ImNqb2ljdHMxYjA1bDAzcW03Zjd0cHBsbXMifQ.rL9QPLvi0kLP3DzLt1PQBA',
+    visible: true,
+    base: true,
+  };
+  const xyzLayer = map.addLayer('xyz', xyzOpts);
+}
+
+export function addDrawingLayer(map, value) {
+  const opts = {
+    title: 'features',
+    ...(value && { geojson: value }),
+    color: 'blue',
+    visible: true,
+  };
+  // const layer = map.addLayer('vector', opts);
+  // map.zoomToLayer(layer);
+
+  // map.addBehavior('edit', { layer });
+  // map.addBehavior('measure', { layer });
+  // map.zoomToLayer(layer);
+  // return layer;
+  return null;
+}
+
+export function getNextValue(geojson) {
+  const geojsonIsEmpty = geojson && geojson.features && geojson.features.length === 0;
+  return geojsonIsEmpty ? null : geojson;
+}
 
 export default {
   mixins: [baseQuestionComponent],
+  components: {
+    appControlLabel,
+    appControlHint,
+    appControlMoreInfo,
+  },
   data() {
     return {
       layer: null,
       map: null,
-      mapId: `farmos-map-${Math.floor(Math.random() * 1e3)}`,
+      mapId: `farmos-map-${Math.floor(Math.random() * 1e4)}`,
     };
   },
   mounted() {
@@ -54,48 +102,65 @@ export default {
       });
       this.map = map;
 
-      // Adding an XYZ layer.
-      const xyzOpts = {
-        title: 'mapbox', // defaults to 'xyz'
-        url: 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoib3Vyc2NpIiwiYSI6ImNqb2ljdHMxYjA1bDAzcW03Zjd0cHBsbXMifQ.rL9QPLvi0kLP3DzLt1PQBA', // REQUIRED!
-        visible: true, // defaults to true
-        base: true, // defaults to false
-      };
-      const xyzLayer = map.addLayer('xyz', xyzOpts);
+      addBaseLayer(map);
+      this.layer = addDrawingLayer(map, this.value);
 
-      console.log('created map', this.value);
+      // let layer;
+      // if (this.value) {
+      //   const vopts = {
+      //     title: 'features',
+      //     geojson: this.value,
+      //     color: 'blue',
+      //     visible: true,
+      //   };
+      //   layer = map.addLayer('geojson', vopts);
+      // } else {
+      //   const vopts = {
+      //     title: 'features',
+      //     color: 'blue',
+      //     visible: true,
+      //   };
+      //   layer = map.addLayer('vector', vopts);
+      // }
+      // map.addBehavior('edit', { layer });
+      // map.addBehavior('measure', { layer });
+      // this.layer = layer;
+
       let layer;
-      let wktOpts;
-      if (this.value && this.value) {
-        wktOpts = {
-          title: 'vectors', // defaults to 'wkt'
-          geojson: this.value,
-          color: 'orange', // defaults to 'orange'
-          visible: true, // defaults to true
-        };
-        layer = map.addLayer('geojson', wktOpts);
-      } else {
-        const vopts = {
-          title: 'field', // defaults to 'wkt'
-          color: 'orange', // defaults to 'orange'
-          visible: true, // defaults to true,
-        };
-        layer = map.addLayer('vector', vopts);
-      }
-      map.addBehavior('edit', { layer });
-      map.addBehavior('measure', { layer });
-      this.layer = layer;
-      map.zoomToLayer(layer);
+      // let wktOpts;
+      // if (this.value && this.value) {
+      //   // wktOpts = {
+      //   //   title: 'vectors', // defaults to 'wkt'
+      //   //   geojson: this.value,
+      //   //   color: 'orange', // defaults to 'orange'
+      //   //   visible: true, // defaults to true
+      //   // };
+      //   // layer = map.addLayer('geojson', wktOpts);
+      // } else {
+      //   const vopts = {
+      //     title: 'field', // defaults to 'wkt'
+      //     color: 'orange', // defaults to 'orange'
+      //     visible: true, // defaults to true,
+      //   };
+      //   // layer = map.addLayer('vector', vopts);
+      //   map.
+      // }
+      // map.addBehavior('edit', { layer });
+      // map.addBehavior('measure', { layer });
 
-      const mapChangeHandler = (geojson) => {
-        this.changed(geojson);
-        // Reset question value to null if geojson feature collection is empty
-        // This ensures that question's required flag will work correctly
-        if (geojson && geojson.features && geojson.features.length === 0) {
-          this.changed(null);
-        }
+      const wkt = 'POLYGON ((-75.53643733263014 42.54424760416683, -75.5360350012779 42.54427527000766, -75.53589016199109 42.54412508386721, -75.53588747978209 42.54302634269183, -75.53643733263014 42.54424760416683))';
+      const wktOpts = {
+        title: 'my-polygon', // defaults to 'wkt'
+        wkt, // REQUIRED!
+        color: 'orange', // defaults to 'orange'
+        visible: true, // defaults to true
       };
+      const wktLayer = map.addLayer('wkt', wktOpts);
 
+      this.layer = layer;
+      // map.zoomToLayer(layer);
+
+      const mapChangeHandler = geojson => this.changed(getNextValue(geojson));
       map.edit.geoJSONOn('drawend', mapChangeHandler);
       map.edit.geoJSONOn('modifyend', mapChangeHandler);
       map.edit.geoJSONOn('translateend', mapChangeHandler);
@@ -103,91 +168,11 @@ export default {
     },
   },
 };
+
+export function hello() {
+  console.log('asdf');
+}
 </script>
 
-<style>
-/* Control color. */
-.ol-control {
-  background-color: rgba(255, 255, 255, 0.25);
-}
-.ol-control svg {
-  fill: green;
-}
-.ol-control button {
-  color: green;
-  background-color: rgba(255, 255, 255, 0.8);
-}
-.ol-control button:hover,
-.ol-control button:focus {
-  background-color: rgba(255, 255, 255, 1);
-}
-.ol-scale-line {
-  background: rgba(255, 255, 255, 0.25);
-}
 
-/* Control backgrounds. */
-.ol-geocoder .gcd-gl-btn {
-  background-image: none;
-}
-.ol-geocoder .gcd-gl-btn:after {
-  content: "\1f50d";
-}
-.layer-switcher > button {
-  background-size: 1.5em 1.5em;
-}
-
-/* Control sizing. */
-.ol-control button,
-.ol-geocoder .gcd-gl-btn {
-  height: 1.75em;
-  width: 1.75em;
-}
-.ol-geocoder .gcd-gl-control {
-  width: 2.4em;
-  height: 2.4em;
-}
-.ol-geocoder .gcd-gl-expanded {
-  width: 16em;
-  height: 2.4em;
-}
-
-/* Control positioning. */
-.ol-geolocate.ol-control {
-  left: 3.5em;
-}
-.ol-geocoder.gcd-gl-container {
-  top: 0.5em;
-  left: 6.5em;
-}
-.ol-geocoder .gcd-gl-input {
-  left: 3em;
-}
-.ol-edit.ol-control {
-  top: 5.5em;
-}
-.ol-rotate.ol-control {
-  right: 3.5em;
-}
-.layer-switcher {
-  top: 3.5em;
-}
-
-.layer-switcher ul {
-  /* margin: 0; */
-}
-
-.layer-switcher ul li {
-  display: flex;
-  align-items: center;
-}
-
-
-.hide-polygon .ol-edit-polygon,
-.hide-point .ol-edit-point,
-.hide-line .ol-edit-line,
-.hide-circle .ol-edit-circle,
-.hide-modify .ol-edit-modify,
-.hide-move .ol-edit-move {
-  display: none;
-}
-</style>
+<style src="./GeoJSON.css" />
