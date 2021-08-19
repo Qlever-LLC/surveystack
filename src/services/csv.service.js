6c5704af8e38a398af1711f6bbf1394bd34ce51c
Manuel Di Cerbo <mdc+git@serverbox.ch>
2021-08-19 17:57:12 +0200

    attempt to fix issue where .value is not defined

--- src/services/csv.service.js
+++ src/services/csv.service.js
@@ -100,6 +100,11 @@ function createCsvLegacy(submissions) {
  * @returns updated submission question object
  */
 export function geojsonTransformer(o) {
+  if (!o.value) {
+    return {
+      ...o,
+    };
+  }
   return {
     ...o,
     value: {
