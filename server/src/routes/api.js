fb5026dbbc56c6042625cf0ed79990541778ac3a
Brian Kockritz <7691852+cryinbockritz@users.noreply.github.com>
2021-10-11 23:24:07 -0400

    remove commented out code

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -56,7 +56,6 @@ router.post('/auth/reset-password', catchErrors(authController.resetPassword));
 /** Group */
 router.get('/groups', catchErrors(groupController.getGroups));
 router.get('/groups/by-path*', catchErrors(groupController.getGroupByPath));
-//router.get('/groups/:id/users', catchErrors(groupController.getUsers));
 router.get('/groups/:id', catchErrors(groupController.getGroupById));
 router.post('/groups', assertAuthenticated, catchErrors(groupController.createGroup));
 router.put(
