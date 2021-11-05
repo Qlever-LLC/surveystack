aa981784b65c4f27e5daa3b4fbe63ee8b6d7588c
Polgár András <azazdeaz@gmail.com>
2021-11-05 19:24:16 +0100

    Fix using URL.createObjectURL in tests

--- client/tests/setup.js
+++ client/tests/setup.js
@@ -3,3 +3,8 @@ import Vuetify from 'vuetify';
 import '@testing-library/jest-dom';
 
 Vue.use(Vuetify);
+
+window.scrollTo = () => {};
+
+// until this gets closed https://github.com/jsdom/jsdom/issues/1721
+global.URL.createObjectURL = jest.fn();
