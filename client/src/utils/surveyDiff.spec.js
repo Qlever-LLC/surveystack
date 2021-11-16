104fd5b137214ad482f5792e5c4dbd29e8af7e26
Polgár András <azazdeaz@gmail.com>
2021-11-16 14:57:09 +0100

    surveyDiff: update tests for diffSurveyVersions

--- client/src/utils/surveyDiff.spec.js
+++ client/src/utils/surveyDiff.spec.js
@@ -1,5 +1,3 @@
-// TODO cleanup the tests once the requirements are finalized
-
 import { normalizedSurveyControls, diffControls, changeType, diffSurveyVersions } from './surveyDiff';
 import { cloneDeep, find, uniqueId, set } from 'lodash';
 import { createControlInstance } from './surveyConfig';
@@ -231,23 +229,69 @@ describe('surveyDiff', () => {
     });
   });
   describe('diffSurveyVersions', () => {
-    it('works', () => {
+    it('detects change', () => {
       const oldNum = createControlInstance({ type: 'number', name: 'number_1', label: 'foo' });
       const newNum = cloneDeep(oldNum);
       newNum.name += 'changed';
 
       const diff = diffSurveyVersions([oldNum], [newNum]);
 
-      expect(diff).toHaveLength(1);
-      const numDiff = diff[0];
-      expect(numDiff).toHaveProperty('changeType', changeType.CHANGED);
-      expect(numDiff).toHaveProperty('matchId', oldNum.id);
-      expect(numDiff).toHaveProperty('diff.name.changeType', changeType.CHANGED);
-      expect(numDiff).toHaveProperty('diff.name.oldValue', oldNum.name);
-      expect(numDiff).toHaveProperty('diff.name.newValue', newNum.name);
-      expect(numDiff).toHaveProperty('diff.label.changeType', changeType.UNCHANGED);
+      expect(diff).toMatchObject([
+        {
+          changeType: changeType.CHANGED,
+          newControl: newNum,
+          newChildIndex: 0,
+          newPath: newNum.name,
+          newParentId: null,
+          oldControl: oldNum,
+          oldChildIndex: 0,
+          oldPath: oldNum.name,
+          oldParentId: null,
+          matchId: newNum.id,
+          diff: {
+            name: {
+              changeType: changeType.CHANGED,
+              oldValue: oldNum.name,
+              newValue: newNum.name,
+            },
+            label: {
+              changeType: changeType.UNCHANGED,
+            },
+          },
+        },
+      ]);
     });
-    it('withMatrices', () => {
+    it('detects addition', () => {
+      const num = createControlInstance({ type: 'number', name: 'number_1', label: 'foo' });
+
+      const diff = diffSurveyVersions([], [num]);
+
+      expect(diff).toMatchObject([
+        {
+          changeType: changeType.ADDED,
+          newControl: num,
+          newChildIndex: 0,
+          newPath: num.name,
+          newParentId: null,
+        },
+      ]);
+    });
+    it('detects removal', () => {
+      const num = createControlInstance({ type: 'number', name: 'number_1', label: 'foo' });
+
+      const diff = diffSurveyVersions([num], []);
+
+      expect(diff).toMatchObject([
+        {
+          changeType: changeType.REMOVED,
+          oldControl: num,
+          oldChildIndex: 0,
+          oldPath: num.name,
+          oldParentId: null,
+        },
+      ]);
+    });
+    it('correctly diffs matrix content', () => {
       const oldMat = createControlInstance({ type: 'matrix', name: 'mat_1' });
       oldMat.options.source.content[0].label = 'Col 1';
       const newMat = cloneDeep(oldMat);
@@ -256,43 +300,53 @@ describe('surveyDiff', () => {
 
       const diff = diffSurveyVersions([oldMat], [newMat]);
 
-      expect(diff).toHaveLength(1);
-      const matDiff = diff[0];
-      expect(matDiff).toHaveProperty('changeType', changeType.CHANGED);
-      expect(matDiff).toHaveProperty(['diff', 'options.source.config.addRowLabel', 'changeType'], changeType.CHANGED);
-      expect(matDiff).toHaveProperty(
-        ['diff', 'options.source.config.addRowLabel', 'oldValue'],
-        oldMat.options.source.config.addRowLabel
-      );
-      expect(matDiff).toHaveProperty(
-        ['diff', 'options.source.config.addRowLabel', 'newValue'],
-        newMat.options.source.config.addRowLabel
-      );
-      expect(matDiff).toHaveProperty('diff.name.changeType', changeType.UNCHANGED);
-      expect(matDiff).toHaveProperty(['diff', 'options.source.content[0].label', 'changeType'], changeType.CHANGED);
-      expect(matDiff).toHaveProperty(
-        ['diff', 'options.source.content[0].label', 'oldValue'],
-        oldMat.options.source.content[0].label
-      );
-      expect(matDiff).toHaveProperty(
-        ['diff', 'options.source.content[0].label', 'newValue'],
-        newMat.options.source.content[0].label
-      );
+      expect(diff).toMatchObject([
+        {
+          changeType: changeType.CHANGED,
+          newControl: newMat,
+          newChildIndex: 0,
+          newPath: newMat.name,
+          newParentId: null,
+          oldControl: oldMat,
+          oldChildIndex: 0,
+          oldPath: oldMat.name,
+          oldParentId: null,
+          matchId: newMat.id,
+          diff: {
+            'options.source.config.addRowLabel': {
+              changeType: changeType.CHANGED,
+              oldValue: oldMat.options.source.config.addRowLabel,
+              newValue: newMat.options.source.config.addRowLabel,
+            },
+            name: {
+              changeType: changeType.UNCHANGED,
+            },
+            'options.source.content[0].label': {
+              changeType: changeType.CHANGED,
+              oldValue: oldMat.options.source.content[0].label,
+              newValue: newMat.options.source.content[0].label,
+            },
+          },
+        },
+      ]);
     });
-    // TODO 'selectSingle', 'selectMultiple'
     describe('works with path ids', () => {
-      const num1 = createControlInstance({ type: 'number', name: 'number_1', label: 'foo' });
-      const num2 = createControlInstance({ type: 'number', name: 'number_2', label: 'foo' });
-      const group1 = createControlInstance({ type: 'group', name: 'group_1', children: [num2] });
-      const oldControls = [num1, group1];
-      const newControls = cloneDeep(oldControls);
-      changeAllControlIds(newControls);
+      const createDiff = () => {
+        const num1 = createControlInstance({ type: 'number', name: 'number_1', label: 'foo' });
+        const num2 = createControlInstance({ type: 'number', name: 'number_2', label: 'foo' });
+        const group1 = createControlInstance({ type: 'group', name: 'group_1', children: [num2] });
+        const oldControls = [num1, group1];
+        const newControls = cloneDeep(oldControls);
+        changeAllControlIds(newControls);
 
-      newControls[0] = createControlInstance({ type: 'string', name: num1.name });
-      newControls[1].children[0].label = 'baz';
-      const diff = diffSurveyVersions(oldControls, newControls, { useControlPathAsId: true });
+        newControls[0] = createControlInstance({ type: 'string', name: num1.name });
+        newControls[1].children[0].label = 'baz';
+        const diff = diffSurveyVersions(oldControls, newControls, { useControlPathAsId: true });
+        return { diff, oldControls, newControls };
+      };
 
       it('matches controls with the same path', () => {
+        const { diff, oldControls, newControls } = createDiff();
         const path = 'group_1.number_2';
         const num2Diff = find(diff, { newPath: path });
         expect(num2Diff).toHaveProperty('changeType', changeType.CHANGED);
@@ -305,6 +359,7 @@ describe('surveyDiff', () => {
       });
 
       it('treats type change as "remove" and "add', () => {
+        const { diff, oldControls, newControls } = createDiff();
         const num1OldDiff = find(diff, { oldPath: 'number_1' });
         expect(num1OldDiff).toHaveProperty('changeType', changeType.REMOVED);
         expect(num1OldDiff).toHaveProperty('oldControl.type', oldControls[0].type);
