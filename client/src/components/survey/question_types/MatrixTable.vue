3eb291545b266a2db0d6accdaaace702b641b613
Polgár András <azazdeaz@gmail.com>
2021-11-20 16:57:36 +0100

    MatrixTable: make cols fixable

--- client/src/components/survey/question_types/MatrixTable.vue
+++ client/src/components/survey/question_types/MatrixTable.vue
@@ -1,32 +1,41 @@
 <template>
-  <div class="wrap">
+  <div class="wrap" :style="cssVariables">
     <div ref="header" class="mt-heading mt-row">
       <div
-        class="caption font-weight-bold text--secondary px-4 mt-cell mt-header"
+        class="caption font-weight-bold text--secondary px-4 mt-cell mt-header mt-divider"
         v-for="(header, colIdx) in headers"
         :key="colIdx"
         :style="colStyles[colIdx]"
       >
-        <slot name="header-cell" v-bind:header="header" v-bind:colIdx="colIdx">
-          {{ colIdx }}
-        </slot>
+        <div class="mt-full" :class="{ 'mt-fix-left': fixCols[colIdx] }">
+          <slot name="header-cell" v-bind:header="header" v-bind:colIdx="colIdx">
+            {{ colIdx }}
+          </slot>
+        </div>
       </div>
       <div :style="headerLeftSpacerStyles" />
     </div>
     <v-virtual-scroll ref="body" v-scroll.self="onScroll" :items="rowsWithId" height="300" item-height="64">
       <template v-slot="{ item }">
         <div class="mt-row">
-          <div class="px-1 mt-cell" v-for="(header, colIdx) in headers" :key="colIdx" :style="colStyles[colIdx]">
-            <slot name="row-cell" v-bind:header="header" v-bind:row="item" v-bind:colIdx="colIdx">
-              {{ ' / ' + colIdx }}
-            </slot>
+          <div
+            class="px-1 mt-cell  mt-divider"
+            v-for="(header, colIdx) in headers"
+            :key="colIdx"
+            :style="colStyles[colIdx]"
+          >
+            <div class="mt-full" :class="{ 'mt-fix-left': fixCols[colIdx] }">
+              <slot name="row-cell" v-bind:header="header" v-bind:row="item" v-bind:colIdx="colIdx">
+                {{ ' / ' + colIdx }}
+              </slot>
+            </div>
           </div>
           <div
             v-if="$scopedSlots['row-actions']"
             class="mt-fixed-right px-1 mt-cell flex-grow-0 flex-shrink-0"
             :style="actionBlockStyles"
           >
-            <div :style="actionStickStyles" class="d-flex align-center">
+            <div class="mt-fix-right mt-fill mt-divider d-flex align-center">
               <slot name="row-actions" v-bind:rowIdx="item.id"> </slot>
             </div>
           </div>
@@ -72,6 +81,9 @@ export default {
     rowsWithId() {
       return this.rows.map((row, id) => ({ ...row, id }));
     },
+    fixCols() {
+      return [true];
+    },
     colStyles() {
       return this.headers.map(({ scaleWidth = 100, type }) => ({
         flexBasis: `${defaultColumnWidth(type) * (scaleWidth / 100)}px`,
@@ -84,12 +96,14 @@ export default {
         flexShrink: 0,
       };
     },
-    actionStickStyles() {
+    stickRight() {
       return {
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'background',
         transform: `translateX(-${this.scrollRight}px)`,
+      };
+    },
+    stickLeft() {
+      return {
+        transform: `translateX(${this.scrollLeft}px)`,
       };
     },
     headerLeftSpacerStyles() {
@@ -99,12 +113,19 @@ export default {
         flexShrink: 0,
       };
     },
+    cssVariables() {
+      return {
+        '--mt-scroll-left': `${this.scrollLeft}px`,
+        '--mt-scroll-right': `${-this.scrollRight}px`,
+      };
+    },
   },
   methods: {
     onScroll(e) {
       this.$refs.header.scrollLeft = e.target.scrollLeft;
       this.scrollLeft = e.target.scrollLeft;
-      this.scrollRight = e.target.scrollLeftMax - e.target.scrollLeft;
+      const scrollLeftMax = e.target.scrollWidth - e.target.clientWidth;
+      this.scrollRight = scrollLeftMax - e.target.scrollLeft;
     },
   },
   watch: {
@@ -141,9 +162,14 @@ export default {
 .mt-header {
   height: 48px;
   line-height: 48px;
+}
+
+.mt-divider {
   border-bottom-color: rgba(0, 0, 0, 0.12);
   border-bottom-style: solid;
-  border-bottom-width: 0px;
+  border-bottom-width: 1px;
+  border-spacing: 0;
+  box-sizing: border-box;
 }
 
 .mt-heading {
@@ -154,8 +180,20 @@ export default {
   overflow-x: hidden;
 }
 
-.mt-fixed-right {
-  /* position: sticky;
-  right: 0px; */
+.mt-fill {
+  width: 100%;
+  height: 100%;
+  background-color: whitesmoke;
+}
+
+.mt-fix-right {
+  transform: translateX(var(--mt-scroll-right));
+}
+
+.mt-fix-left {
+  transform: translateX(var(--mt-scroll-left));
+  /* keep it above the other input cells */
+  z-index: 1;
+  position: relative;
 }
 </style>
