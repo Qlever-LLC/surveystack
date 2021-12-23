dd9e5d5ec1588399c40d27c7d8dba0f15ecc5ea2
Polgár András <azazdeaz@gmail.com>
2021-12-23 17:59:47 +0100

    Fix timeout for CI tests

--- server/tests/setupInMemoryMongo.js
+++ server/tests/setupInMemoryMongo.js
@@ -3,13 +3,14 @@ const { connectDatabase, getDb } = require('../src/db');
 
 let mongod;
 
+const CREATE_DB_TIMEOUT = 60000;
 beforeAll(async () => {
   // This will create an new instance of "MongoMemoryServer" and automatically start it
   mongod = await MongoMemoryServer.create();
   // Prepare the env for connectDatabase
   process.env.DATABASE_URL = mongod.getUri();
   await connectDatabase();
-});
+}, CREATE_DB_TIMEOUT);
 
 afterEach(async () => {
   // clean up the DB after each test
