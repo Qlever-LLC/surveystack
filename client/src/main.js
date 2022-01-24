0054fff1945d98346ef9f5aba98f91d461b188aa
Polgár András <azazdeaz@gmail.com>
2022-01-24 16:36:55 +0100

    Add basic sentry integration

--- client/src/main.js
+++ client/src/main.js
@@ -20,19 +20,33 @@ import appControlHint from '@/components/survey/drafts/ControlHint.vue';
 import appControlMoreInfo from '@/components/survey/drafts/ControlMoreInfo.vue';
 import appControlError from '@/components/survey/drafts/ControlError.vue';
 
+// TODO delete me!
+console.log('process.env.VUE_APP_VERSION', process.env.VUE_APP_VERSION);
+console.log('process.env.VUE_APP_COMMIT_SHORT_SHA', process.env.VUE_APP_COMMIT_SHORT_SHA);
+console.log('process.env.VUE_APP_API_URL', process.env.VUE_APP_API_URL);
+console.log('process.env.VUE_APP_SENTRY_DSN', process.env.VUE_APP_SENTRY_DSN);
+console.log('process.env.VUE_APP_ENVIRONMENT', process.env.VUE_APP_ENVIRONMENT);
+
 Sentry.init({
   Vue,
-  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+  environment: process.env.VUE_APP_ENVIRONMENT,
+  release: `survey-stack-client@${process.env.VUE_APP_VERSION}/${process.env.VUE_APP_COMMIT_SHORT_SHA || 'unknown'}`,
+  dsn: process.env.VUE_APP_SENTRY_DSN,
   integrations: [
     new Integrations.BrowserTracing({
       routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracingOrigins: ['localhost', 'my-site-url.com', /^\//],
+      tracingOrigins: [process.env.VUE_APP_API_URL, /^\//],
     }),
   ],
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  debug: false,
+  tracesSampleRate: Number.parseFloat(process.env.VUE_APP_SENTRY_TRACES_SAMPLE_RATE) || 1.0,
+  tracingOptions: {
+    trackComponents: true,
+  },
+  // Vue specific
+  logErrors: process.env.VUE_APP_ENVIRONMENT === 'production' ? false : true,
+  attachProps: true,
+  attachStacktrace: true,
 });
 
 Vue.component('app-control-label', appControlLabel);
