a6169aad09739387d1d3ec1f459dcde92cc327c1
Polgár András <azazdeaz@gmail.com>
2022-01-28 16:04:59 +0100

    Sentry integration: clean up and add comments

--- client/src/plugins/sentry.js
+++ client/src/plugins/sentry.js
@@ -1,21 +1,38 @@
 import * as Sentry from '@sentry/vue';
 import { Integrations } from '@sentry/tracing';
 
-// TODO delete me!
-console.log('process.env.VUE_APP_VERSION', process.env.VUE_APP_VERSION);
-console.log('process.env.VUE_APP_COMMIT_SHA', process.env.VUE_APP_COMMIT_SHA);
-console.log('process.env.VUE_APP_API_URL', process.env.VUE_APP_API_URL);
-console.log('process.env.VUE_APP_SENTRY_DSN', process.env.VUE_APP_SENTRY_DSN);
-console.log('process.env.VUE_APP_ENVIRONMENT', process.env.VUE_APP_ENVIRONMENT);
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-console.log('process.env.VUE_APP_SENTRY_TRACES_SAMPLE_RATE', process.env.VUE_APP_SENTRY_TRACES_SAMPLE_RATE);
+// NOTE: you can send traces from your localhost by adding these variables to client/.env.local
+//  VUE_APP_SENTRY_DSN="Copy the DNS from https://sentry.io/settings/our-sci/projects/survey-stack/keys/"
+//  VUE_APP_SENTRY_ENV="YOURNAME-development"
 
+// Start error tracking and performance monitoring with Sentry.io
 export const startSentry = (Vue, store, router) => {
+  // Parameters
+
+  // User for filtering traces
+  const environment = process.env.VUE_APP_SENTRY_ENV || process.env.NODE_ENV || 'development';
+  console.log({ environment }, process.env.VUE_APP_SENTRY_ENV, process.env.NODE_ENV);
+  // Enables differentiating between different versions of the app
+  // The env variable is set on GitLab CI (see gitlab-ci.yaml)
+  const release = process.env.VUE_APP_COMMIT_SHA || 'unknown';
+  // Sets the chance that a log will be sent to Sentry.io
+  // Useful to lower the amount of logs sent in production
+  // Set in the GitLab CI variables: https://gitlab.com/our-sci/software/surveystack/-/settings/ci_cd
+  const tracesSampleRate = Number.parseFloat(process.env.VUE_APP_SENTRY_TRACES_SAMPLE_RATE) || 1.0;
+  // Sentry.io sink endpoint.
+  // Set in the GitLab CI variables: https://gitlab.com/our-sci/software/surveystack/-/settings/ci_cd
+  const dsn = process.env.VUE_APP_SENTRY_DSN;
+
+  if (!dsn) {
+    console.warn('VUE_APP_SENTRY_DSN is not set. Skip tracing to Sentry.io');
+    return;
+  }
+
   Sentry.init({
     Vue,
-    environment: process.env.NODE_ENV,
-    release: process.env.VUE_APP_COMMIT_SHA || 'unknown',
-    dsn: process.env.VUE_APP_SENTRY_DSN,
+    environment,
+    release,
+    dsn,
     integrations: [
       new Integrations.BrowserTracing({
         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
@@ -23,7 +40,7 @@ export const startSentry = (Vue, store, router) => {
       }),
     ],
     debug: false,
-    tracesSampleRate: Number.parseFloat(process.env.VUE_APP_SENTRY_TRACES_SAMPLE_RATE) || 1.0,
+    tracesSampleRate,
     tracingOptions: {
       trackComponents: true,
     },
@@ -37,6 +54,7 @@ export const startSentry = (Vue, store, router) => {
   store.watch(
     (_, getters) => getters['auth/user'],
     ({ _id: id, name: username, email } = {}) => {
+      // include user info in the traces
       Sentry.setUser({ id, email, username });
     },
     { immediate: true }
@@ -50,6 +68,7 @@ export const startSentry = (Vue, store, router) => {
       return groups.find((item) => item._id === activeGroupId);
     },
     ({ id, name } = {}) => {
+      // include group info in the traces
       Sentry.setContext('group', { id, name });
     },
     { immediate: true }
