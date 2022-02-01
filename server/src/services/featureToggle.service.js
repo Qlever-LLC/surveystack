3e5969fd06272f4d2b6484b51e76e9fe49a8e8a9
Polgár András <azazdeaz@gmail.com>
2022-02-01 12:37:33 +0100

    FeatureToggles: handle when env vars are not set

--- server/src/services/featureToggle.service.js
+++ server/src/services/featureToggle.service.js
@@ -2,28 +2,34 @@ import { createApp } from '@unleash/proxy';
 import Client from '@unleash/proxy/dist/client';
 import { createProxyConfig } from '@unleash/proxy/dist/config';
 
-// TODO handle when env variables not set
+// Disable service when env variables are not set (to prevent flooding the consol with errors in development)
+const ENABLED = process.env.UNLEASH_URL && process.env.UNLEASH_INSTANCE_ID;
+
+if (!ENABLED) {
+  console.warn('ENV vars missing for the feature toggles. See featureToggles.md for details.');
+}
 
 const config = {
   unleashUrl: process.env.UNLEASH_URL,
   unleashApiToken: '_',
   unleashInstanceId: process.env.UNLEASH_INSTANCE_ID,
-  clientKeys: ['proxy-secret', 'another-proxy-secret', 's1'],
+  clientKeys: ['proxy-secret'],
   refreshInterval: 1000,
-  // logLevel: 'info',
-  // projectName: 'order-team',
   environment: 'development',
 };
 
-const client = new Client(createProxyConfig(config));
+const client = ENABLED ? new Client(createProxyConfig(config)) : null;
 
 // Resolves when the client loaded the initial toggle states
-const waitReadyPromise = new Promise((resolve) => {
-  client.on('ready', () => resolve());
-});
+const waitReadyPromise = ENABLED
+  ? new Promise((resolve) => {
+      client.on('ready', () => resolve());
+    })
+  : null;
 
+const noopMiddleware = (_, __, next) => next();
 // This should be added to the express app, so the client can query this endpoint instead of the external service.
-export const unleashProxyApp = createApp(config, client);
+export const unleashProxyApp = ENABLED ? createApp(config, client) : noopMiddleware;
 
 /**
  * Makes sure that the toggles are loaded and reads the state of the requested feature
@@ -32,6 +38,9 @@ export const unleashProxyApp = createApp(config, client);
  * @returns {Promise<boolean>} state of a feature toggle
  */
 export const isToggleOn = async (toggleName, userId) => {
+  if (!ENABLED) {
+    return false;
+  }
   await waitReadyPromise;
   const context = { userId };
   return client
