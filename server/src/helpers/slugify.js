8d38ba5f1382622ea50e2940674ab3814fccedd2
mhaerdi@gmx.ch <ZS6qdauGAw2zQCMzccV4>
2022-02-09 11:29:52 +0100

    add tests

--- server/src/helpers/slugify.js
+++ server/src/helpers/slugify.js
@@ -2,9 +2,9 @@ export default function slugify(text) {
   return text
     .toString()
     .toLowerCase()
-    .replace(/\s+/g, '_') // Replace spaces with -
+    .replace(/\s+/g, '_') // Replace spaces with _
     .replace(/[^\w_]+/g, '') // Remove all non-word chars
-    .replace(/__+/g, '-') // Replace multiple - with single -
+    .replace(/__+/g, '-') // Replace multiple _ with single -
     .replace(/^_+/, '') // Trim - from start of text
     .replace(/_+$/, ''); // Trim - from end of text
 }
