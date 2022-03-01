15c83f8cda6fabdc8088f83afa04873e449d3bcf
Manuel Di Cerbo <mdc+git@serverbox.ch>
2022-03-01 09:30:42 +0100

    fixes to storybook and adding ci

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@socheatsok78/storybook-addon-vuetify',
-    '@storybook/addon-interactions',
   ],
   framework: '@storybook/vue',
 };
