2a7206d18d8d8768e3eb07a00d164f2b98f097c2
mhaerdi@gmx.ch <ZS6qdauGAw2zQCMzccV4>
2022-03-07 09:07:28 +0100

    add file drop feature

--- client/src/components/survey/question_types/File.vue
+++ client/src/components/survey/question_types/File.vue
@@ -3,10 +3,13 @@
     <app-control-label :value="control.label" :redacted="redacted" :required="required" />
     <app-control-hint :value="control.hint" />
     <v-row>
-      <div class="col-12">
+      <div class="col-12" @drop.prevent="onDrop" @dragover.prevent="onDragOver">
         <v-file-input
           id="fileChooser"
           name="fileChooser"
+          :placeholder="
+            $vuetify.breakpoint.mobile || forceMobile ? 'tap here to upload' : 'click or drop here to upload'
+          "
           outlined
           show-size
           :counter="control.options.source.allowMultiple"
@@ -75,20 +78,63 @@ export default {
         // To prevent this, use additional array previousFiles as a backup and restore previously selected files
         const newFiles = this.$el.querySelector('#fileChooser').files;
         newFiles.forEach((file) => {
-          if (this.previousFiles.length === 0) {
-            this.previousFiles.push(file);
-          } else {
-            const index = this.previousFiles.findIndex((x) => x.name === file.name);
-            if (index >= 0) {
-              this.previousFiles.splice(index, 1);
-            }
-            this.previousFiles.push(file);
-          }
+          this.addFile(file, this.previousFiles);
         });
         this.files = this.previousFiles;
       }
 
       this.changed(this.files.length ? this.files : null);
+    },
+    addFile(file, destArray) {
+      if (destArray.length === 0) {
+        destArray.push(file);
+      } else {
+        //prevent duplicates
+        const index = destArray.findIndex((x) => x.name === file.name);
+        if (index >= 0) {
+          destArray.splice(index, 1);
+        }
+        destArray.push(file);
+      }
+    },
+    onDrop(event) {
+      let newFiles = [];
+      if (event.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        event.dataTransfer.items.forEach((item) => {
+          if (item.kind === 'file' && this.isMimeTypeAllowed(this.control.options.source.types, item.type)) {
+            newFiles.push(item.getAsFile());
+          } else {
+            alert('drop not allowed, only file a type of ' + this.control.options.source.types);
+          }
+        });
+      } else {
+        // Use DataTransfer interface to access the file(s)
+        newFiles.push(...event.dataTransfer.files);
+      }
+
+      if (newFiles && newFiles.length > 0) {
+        if (this.control.options.source.allowMultiple) {
+          newFiles.forEach((file) => {
+            this.addFile(file, this.files);
+          });
+        } else {
+          this.files = [newFiles[0]];
+        }
+
+        this.changed(this.files.length ? this.files : null);
+      }
+    },
+    onDragOver(event) {
+      event.dataTransfer.dropEffect = 'copy';
+    },
+    isMimeTypeAllowed(allowedTypes, type) {
+      if (!allowedTypes || allowedTypes === 0) {
+        //no type restrictions
+        return true;
+      } else {
+        return !!allowedTypes.find((allowedType) => type.match(allowedType));
+      }
     },
     clear() {
       this.previousFiles = [];
