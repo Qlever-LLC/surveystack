6d6c106e109c480b555344abf0d48f1ee775d5bc
Polgár András <azazdeaz@gmail.com>
2022-03-24 14:43:47 +0100

    set cookie.secure = false

--- server/src/constants.js
+++ server/src/constants.js
@@ -2,6 +2,6 @@ export const GROUP_PATH_DELIMITER = '/';
 
 export const cookieOptions = {
   expires: new Date(Date.now() + 1000 * 3600 * 24 * 14),
-  secure: process.env.NODE_ENV === 'production',
+  secure: false,//process.env.NODE_ENV === 'production',
   sameSite: 'None',
 }; // expire after 14 days
