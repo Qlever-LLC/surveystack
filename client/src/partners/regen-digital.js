d080978c4eaa2738823eca17a73303d753bad184
Manuel Di Cerbo <mdc+git@serverbox.ch>
2022-04-25 16:17:30 +0200

    add regen digital white label

---
+++ client/src/partners/regen-digital.js
@@ -0,0 +1,20 @@
+export default {
+  name: 'Regen Farmers Mutual',
+  domain: 'regen-digital',
+  id: '6171ca059d62e10001356510',
+  slug: 'regen-farmers-mutual',
+  path: '/regen-farmers-mutual/',
+  logo: '/partners/regen-digital/img/logo.png',
+  themes: {
+    light: {
+      primary: '#7cb0d7',
+      secondary: '#7cb0d7',
+      accent: '#FF5722',
+      error: '#f44336',
+      warning: '#ffc107',
+      info: '#7cb0d7',
+      success: '#8bc34a',
+      appbar: '#FFFFFF',
+    },
+  },
+};
