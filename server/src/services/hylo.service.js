6e0f7e38061e71f6422f2bda7c514c7e06c4b7d1
Polgár András <azazdeaz@gmail.com>
2022-05-04 16:18:20 +0200

    Implement basic apiCompose handle for hylo

--- server/src/services/hylo.service.js
+++ server/src/services/hylo.service.js
@@ -1,13 +1,11 @@
 /* eslint-disable no-unreachable */
 import boom from '@hapi/boom';
 import axios from 'axios';
-import https from 'https';
+import { ObjectId } from 'mongodb';
 import querystring from 'querystring';
 
 import * as utils from '../helpers/surveys';
 
-import { hasPermission } from './farmos-2/apiCompose';
-import { aggregator } from './farmos-2/aggregator';
 import { db } from '../db';
 import { request, gql } from 'graphql-request';
 
@@ -85,43 +83,49 @@ const queryHyloUser = async (email) => {
   );
 };
 
+const FRAGMENT_GROUP_DETAILS = gql`
+  fragment GroupDetails on Group {
+    id
+    name
+    slug
+    members {
+      items {
+        id
+        name
+        hasRegistered
+      }
+    }
+  }
+`;
+
 const queryHyloGroup = async (slug) => {
   return gqlRequest(
     gql`
       query ($id: ID, $slug: String) {
         group(id: $id, slug: $slug) {
-          id
-          name
-          slug
-          members {
-            items {
-              id
-              name
-              hasRegistered
-            }
-          }
+          ...GroupDetails
         }
       }
+      ${FRAGMENT_GROUP_DETAILS}
     `,
     { slug }
   );
 };
 
-const createHyloGroup = async (slug, hyloUserId) => {
+const createHyloGroup = async ({ name, slug, farm_email, hyloUserId }) => {
   return gqlRequest(
     gql`
       mutation ($data: GroupInput, $asUserId: ID) {
-        createGroup(data: $data, asUserId: $asUserId) {
-          id
-          name
-          slug
+        group: createGroup(data: $data, asUserId: $asUserId) {
+          ...GroupDetails
         }
       }
+      ${FRAGMENT_GROUP_DETAILS}
     `,
     {
       data: {
         accessibility: 1,
-        name: 'Test Group',
+        name,
         slug,
         parentIds: [],
         visibility: 1,
@@ -129,7 +133,8 @@ const createHyloGroup = async (slug, hyloUserId) => {
           {
             type: 'farm-onboarding',
             data: {
-              farm_email: 'test@farm.org',
+              // TODO ask Tibet if this should work?
+              farm_email,
             },
           },
         ],
@@ -139,46 +144,77 @@ const createHyloGroup = async (slug, hyloUserId) => {
   );
 };
 
-const updateHyloGroup = async (hyloGroupId, hyloUserId) => {
+const updateHyloGroup = async ({ name, farm_email, hyloUserId, hyloGroupId }) => {
   return gqlRequest(
     gql`
       mutation ($id: ID, $changes: GroupInput, $asUserId: ID) {
-        updateGroup(id: $id, changes: $changes, asUserId: $asUserId) {
-          id
-          name
-          slug
+        group: updateGroup(id: $id, changes: $changes, asUserId: $asUserId) {
+          ...GroupDetails
         }
       }
+      ${FRAGMENT_GROUP_DETAILS}
     `,
     {
       id: hyloGroupId,
       changes: {
-        name: 'New Name2',
+        name,
+        groupExtensions: [
+          {
+            type: 'farm-onboarding',
+            data: {
+              farm_email,
+            },
+          },
+        ],
       },
       asUserId: hyloUserId,
     }
   );
 };
 
-const addUserToHylo = async (email) => {
-  const user = await db.collection('users').findOne({ email });
+const syncUserWithHylo = async (id) => {
+  const user = await db.collection('users').findOne({ _id: new ObjectId(id) });
   if (!user) {
     throw new Error('TODO');
   }
-  let hyloUser = await createHyloUser(user.email);
+  let hyloUser = await createHyloUser({
+    email: user.email,
+    name: user.name,
+  });
 
   // if user already exists
   if (!hyloUser?.id) {
-    const { data, errors } = queryHyloUser(user.email);
-    if (errors?.length > 0) {
-      throw new Error(`Error on querying hylo user ${errors.map((e) => e.message).join('\n')}`);
-    }
-    hyloUser = data.person;
+    const { person } = await queryHyloUser(user.email);
+    hyloUser = person;
   }
 
   if (!hyloUser?.id) {
     throw new Error("Hylo didn't return an ID for the user");
   }
+
+  return hyloUser;
+};
+
+const syncGroupWithHylo = async ({ name, slug, farm_url, hyloUserId }) => {
+  let group = null;
+  try {
+    group = (await createHyloGroup({ name, slug, farm_url, hyloUserId })).group;
+  } catch (e) {
+    if (
+      !e.response?.errors?.some((e) => e.message === 'A group with that URL slug already exists')
+    ) {
+      console.error(e);
+    }
+  }
+  console.log('group after create', { group });
+  if (!group?.id) {
+    group = (await queryHyloGroup(slug)).group;
+    if (!group?.id) {
+      throw new Error(`Failed to create of read the Hylo group with slug "${slug}`);
+    }
+    group = await updateHyloGroup({ name, farm_url, hyloUserId, hyloGroupId: group.id });
+  }
+  return group;
 };
 
 // getToken().then((t) => console.log('Token', t));
@@ -196,257 +232,83 @@ const addUserToHylo = async (email) => {
 //   return t;
 // });
 
-const config = () => {
-  if (!process.env.FARMOS_AGGREGATOR_URL || !process.env.FARMOS_AGGREGATOR_APIKEY) {
-    console.log('env not set');
-    return;
-  }
-
-  return aggregator(process.env.FARMOS_AGGREGATOR_URL, process.env.FARMOS_AGGREGATOR_APIKEY);
-};
-
-function allowed(instanceName, user) {
-  const url = instanceName;
-  if (typeof url != 'string') {
-    throw boom.badData(`url is not a string: ${url}`);
-  }
-
-  return hasPermission(user, url);
-}
-
-async function flushlogs(instanceName, user, id) {
-  /**
-   * TODO Group Admins should have access
-   */
-
-  const url = instanceName;
-  if (typeof url != 'string') {
-    throw boom.badData(`url is not a string: ${url}`);
-  }
-
-  if (!allowed(instanceName, user)) {
-    throw boom.unauthorized('No Access to farm');
-  }
-
-  const { deleteAllWithData } = config();
-
-  console.log('deleting data', instanceName, id);
-  const res = await deleteAllWithData(instanceName, id);
-  console.log('done');
-  return [res];
-}
-
-async function fetchTerms(instanceName, credentials, user) {
-  const url = instanceName;
-  if (typeof url != 'string') {
-    throw boom.badData(`url is not a string: ${url}`);
-  }
-
-  if (!allowed(instanceName, user)) {
-    throw boom.unauthorized('No Access to farm');
-  }
-
-  // TODO fetch terms
-}
-
-async function execute(aggregator, apiCompose, info, terms, user, submission) {
-  console.log('executing apiCompose', apiCompose);
-
-  const url = apiCompose.url;
-  if (typeof url != 'string') {
-    throw boom.badData(`url is not a string: ${url}`);
-  }
-
-  if (!allowed(url, user)) {
-    throw boom.unauthorized(`User has no access to farm: ${url}`);
-  }
-
-  if (!apiCompose.entity || !apiCompose.entity.type) {
-    throw boom.badData('apiCompose: missing entity or entity.type');
-  }
-
-  const [endpoint, bundle] = apiCompose.entity.type.split('--');
-
-  if (!endpoint || !bundle) {
-    throw boom.badData('apiCompose.entity.type should be in the form endpoint--bundle');
-  }
-
-  // TODO
-  // create log / assets / profile ....
-
-  const payload = { data: apiCompose.entity };
-  payload.data.attributes.data = submission._id;
-
-  return await aggregator.create(url, endpoint, bundle, payload);
-}
-
 export const handle = async ({ submission, survey, user }) => {
   const surveyVersion = submission.meta.survey.version;
 
-  console.log('submission', JSON.stringify(submission, null, 2));
+  console.log('submission-hylo', JSON.stringify(submission, null, 2));
 
   const { controls } = survey.revisions.find((revision) => revision.version === surveyVersion);
   const positions = utils.getControlPositions(controls);
 
-  const farmOsCompose = [];
-  positions.forEach((position) => {
-    const control = utils.getControl(controls, position);
-    if (!control.options.apiCompose || !control.options.apiCompose.enabled) {
-      return;
-    }
-
-    const field = utils.getSubmissionField(submission, survey, position);
-
-    const compose = [];
-
-    if (field.meta.relevant === false) {
-      return;
-    }
-
-    if (!field.meta.apiCompose) {
-      return;
-    }
-
-    if (Array.isArray(field.meta.apiCompose)) {
-      for (const c of field.meta.apiCompose) {
-        compose.push(c);
+  const hyloCompose = positions
+    .map((position) => {
+      const control = utils.getControl(controls, position);
+      if (!control.options.apiCompose || !control.options.apiCompose.enabled) {
+        return [];
       }
-    } else if (typeof field.meta.apiCompose === 'object') {
-      compose.push(field.meta.apiCompose);
-    } else {
-      return;
-    }
 
-    const relevantApiCompose = compose.filter((c) => c.type !== undefined && c.type === 'farmos');
-    if (relevantApiCompose.length === 0) {
-      return;
-    }
+      const field = utils.getSubmissionField(submission, survey, position);
 
-    farmOsCompose.push(...compose);
-  });
+      const compose = [];
 
-  if (farmOsCompose.length === 0) {
-    return [];
-  }
+      if (field.meta.relevant === false) {
+        return [];
+      }
+
+      if (!field.meta.apiCompose) {
+        return [];
+      }
+
+      if (Array.isArray(field.meta.apiCompose)) {
+        for (const c of field.meta.apiCompose) {
+          compose.push(c);
+        }
+      } else if (typeof field.meta.apiCompose === 'object') {
+        compose.push(field.meta.apiCompose);
+      } else {
+        return [];
+      }
+
+      const relevantApiCompose = compose.filter((c) => c.type === 'hylo');
+      if (relevantApiCompose.length === 0) {
+        return [];
+      }
+
+      return compose;
+    })
+    .flat();
 
   const results = [];
-
-  // TODO adapt to farmos 2.0
-
-  const aggregator = config();
-
-  const runSingle = async (apiCompose, info, terms) => {
-    try {
-      const r = await execute(aggregator, apiCompose, info, terms, user, submission);
-
-      if (Array.isArray(r)) {
-        results.push(...r);
-        console.log('results interim after array spread', results);
-        return r;
-      } else {
-        results.push(r);
-        console.log('results interim after single add', results);
-        return [r];
+  for (const { hyloType, body } of hyloCompose) {
+    if (hyloType === 'sync-group') {
+      const { name, slug, farm_email } = body;
+      if (typeof name !== 'string') {
+        throw boom.badData(`name is not a string: ${name}`);
       }
-    } catch (error) {
-      console.log('error in run single', error);
-      results.push({
-        status: 'error',
-        error,
+      if (typeof slug !== 'string') {
+        throw boom.badData(`slug is not a string: ${slug}`);
+      }
+      if (typeof farm_email !== 'string') {
+        throw boom.badData(`farm_email is not a string: ${farm_email}`);
+      }
+
+      console.log('do the thing with', { name, slug, farm_email });
+      console.log('as user', JSON.stringify(user, null, 2));
+
+      const hyloUser = await syncUserWithHylo(user._id);
+      console.log({ hyloUser });
+
+      const hyloGroup = await syncGroupWithHylo({
+        name,
+        slug,
+        farm_email,
+        hyloUserId: hyloUser.id,
       });
-      return null;
-    }
-  };
-
-  // TODO find for each instance relevant farm information
-
-  const farmUrlMap = {};
-  farmOsCompose.forEach((c) => {
-    if (typeof c.url !== 'string') {
-      throw boom.badData(`url is not a string: ${c.url}`);
-    }
-    const items = farmUrlMap[c.url];
-    farmUrlMap[c.url] = items === undefined ? 1 : items + 1;
-  });
-
-  for (const farmUrl of Object.keys(farmUrlMap)) {
-    const res = await flushlogs(farmUrl, user, submission._id);
-    if (res.length > 0) {
-      results.push(res);
+      results.push({ hyloUser, hyloGroup });
     }
   }
-  for (const compose of farmOsCompose) {
-    const info = {};
-    const terms = {};
+  console.log('RESULTS', JSON.stringify(results, null, 2));
 
-    const r = await runSingle(compose, info, terms);
-  }
-
-  console.log('results', results);
+  // TODO does results have an expected format?
   return results;
 };
-
-const testAggregatorConnection = async (url, apiKey) => {
-  const agentOptions = {
-    host: url,
-    port: '443',
-    path: '/',
-    rejectUnauthorized: false,
-  };
-
-  const agent = new https.Agent(agentOptions);
-
-  const r = await axios.get(`https://${url}/api/v2/farms/`, {
-    headers: {
-      accept: 'application/json',
-      'api-key': apiKey,
-    },
-    httpsAgent: agent,
-  });
-
-  if (r.status === 200) {
-    return true;
-  } else {
-    throw Error('unable to connect to aggregator');
-  }
-};
-
-const isFarmosUrlAvailable = async (url, apiKey) => {
-  const agentOptions = {
-    host: 'account.farmos.net',
-    port: '443',
-    path: '/',
-    rejectUnauthorized: false,
-  };
-
-  const agent = new https.Agent(agentOptions);
-
-  try {
-    const r = await axios.post(
-      `https://account.farmos.net/api/v1/utils/validate-farm-url`,
-      {
-        url: `${url}.farmos.net`,
-      },
-      {
-        headers: {
-          accept: 'application/json',
-          apikey: apiKey,
-        },
-        httpsAgent: agent,
-      }
-    );
-
-    if (r.status === 200) {
-      return true;
-    } else {
-      throw Error('unable to connect to aggregator');
-    }
-  } catch (error) {
-    if (error && error.response && error.response.status && error.response.status === 400) {
-      return false;
-    }
-    throw error;
-  }
-};
-
-export default { isFarmosUrlAvailable, handle, testAggregatorConnection };
