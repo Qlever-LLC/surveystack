dbd4619e3adaf7871cdfaf238e89a479272ddaf1
mhaerdi@gmx.ch <ZS6qdauGAw2zQCMzccV4>
2022-06-08 13:07:16 +0200

    - add test case - add fake indexeddb - NOT WORKING YET

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
-  setupFiles: ['jest-canvas-mock'],
+  setupFiles: ['jest-canvas-mock', 'fake-indexeddb/auto'],
   setupFilesAfterEnv: ['./tests/setup.js'],
   clearMocks: true,
   resetModules: true,
--- client/src/components/survey/question_types/File.spec.js
+++ client/src/components/survey/question_types/File.spec.js
@@ -1,6 +1,9 @@
+require('fake-indexeddb/auto');
 import { mount, shallowMount } from '@vue/test-utils';
 import Vuetify from 'vuetify';
 import FileComp from './File.vue';
+import { Blob } from 'buffer';
+import store from '@/store';
 
 const vuetify = new Vuetify();
 
@@ -27,7 +30,18 @@ function getMountOpts(opts = {}) {
       index: 'data.file_1',
     },
     vuetify,
+    store: store,
   };
+}
+
+function getMockFile(fileName) {
+  let content = 'test-data';
+  let contentType = 'text/plain';
+
+  let blob = new Blob([content], { type: contentType });
+  blob['lastModifiedDate'] = '';
+  blob['name'] = fileName;
+  return blob;
 }
 
 describe('File question', () => {
@@ -45,7 +59,19 @@ describe('File question', () => {
       rendersValue(['resources/62398a1e977e7a0001718ee6/test2.jpg', 'resources/62398a1e977e7a0001718fe6/test3.jpg']));
   });
   describe('adding', () => {
-    test.todo('a file with an allowed type is added to value');
+    test('a file with an allowed type is added to value', async () => {
+      const fileResourceKeys = [];
+      const wrapper = mount(FileComp, getMountOpts({ value: [] }));
+      const fileToAdd = getMockFile('test_file.txt');
+      await wrapper.vm.addFile(fileToAdd, fileResourceKeys, false, [
+        { text: 'PDF (.pdf)', value: 'application/pdf' },
+        { text: 'Spreadsheets (.csv)', value: 'text/csv' },
+        { text: 'Image (.gif, .png, .jpg, .jpeg)', value: 'image/*' },
+        { text: 'Plain text (.txt)', value: 'text/plain' },
+      ]);
+      expect(fileResourceKeys[0]).toContain('test_file.txt');
+      //expect(wrapper.vm.$options.propsData.value).toContain(resource);
+    });
     test.todo('two files results in two files if multiple files is allowed');
     test.todo('two files results in one file if multiple files is not allowed ');
     test.todo('a file with an excluded type results in error message');
