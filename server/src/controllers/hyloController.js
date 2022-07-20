99601e0cfaedd363699877972d66a0566a98f812
Polgár András <azazdeaz@gmail.com>
2022-07-20 13:09:53 +0200

    finish invite-to-hylo-group implementation

--- server/src/controllers/hyloController.js
+++ server/src/controllers/hyloController.js
@@ -4,12 +4,13 @@ import Joi from 'joi';
 import joiObjectId from 'joi-objectid';
 Joi.objectId = joiObjectId(Joi);
 
-import _ from 'lodash';
+import _, { get } from 'lodash';
 import boom from '@hapi/boom';
 
 import { gqlRequest } from '../services/hylo/utils';
 import { gql } from 'graphql-request';
 import { addMember, createHyloGroup, upsertHyloUser } from '../services/hylo.service';
+import rolesService from '../services/roles.service';
 
 const validateOrThrow = (schema, _value) => {
   const { value, error } = schema.validate(_value);
@@ -67,6 +68,8 @@ export const getIntegratedHyloGroup = async (req, res) => {
   });
 
   const { groupId } = validateOrThrow(schema, req.params);
+
+  // Find integrated Hylo group
   const mapping = await db
     .collection(COLL_GROUPS_HYLO_MAPPINGS)
     .findOne({ groupId: new ObjectId(groupId) });
@@ -74,6 +77,7 @@ export const getIntegratedHyloGroup = async (req, res) => {
     return res.json(null);
   }
 
+  // Find SurveyStack memberships
   const ssMemberships = await db
     .collection('memberships')
     .aggregate([
@@ -86,26 +90,37 @@ export const getIntegratedHyloGroup = async (req, res) => {
     ])
     .toArray();
 
-  console.log('ssMemberships', JSON.stringify(ssMemberships, null, 2));
+  // Find members on by email
   const queryHyloMembers = gql`
     query SurveyStackMembers {
       ${ssMemberships
         .map(
           ({ user }, i) => `member${i}: person(email:"${user?.email}") {
-        id
-      }`
+            id
+            hasRegistered
+          }`
         )
         .join('\n')}
     }
   `;
-  console.log({ queryHyloMembers });
   const hyloMembers = await gqlRequest(queryHyloMembers);
-  console.log({ hyloMembers });
+  console.log(hyloMembers);
+
+  // Add the SurveyStack user IDs to the Hylo member objects
   const hyloGroup = await loadHyloGroup(mapping.hyloGroupId);
   ssMemberships.forEach((ssMembership, i) => {
     const hyloMember = hyloMembers[`member${i}`];
-    const hyloMemberInGroup =
+    // Only add the user if they are also a member of the Hylo group
+    let hyloMemberInGroup =
       hyloMember && (hyloGroup?.members?.items || []).find((m) => m.id === hyloMember.id);
+
+    // TODO Hylo currently don't show membership status until the user finished registration
+    //   for now, add the user even if it not listed in the Hylo group members
+    if (!hyloMemberInGroup && hyloMember && !hyloMember.hasRegistered) {
+      (hyloGroup?.members?.items || []).push(hyloMember);
+      hyloMemberInGroup = hyloMember;
+    }
+
     if (hyloMemberInGroup) {
       hyloMemberInGroup.surveyStackMembership = ssMembership;
       hyloMemberInGroup.hyloUrl = `${process.env.HYLO_API_URL}/groups/${hyloGroup?.slug}/members/${hyloMember.id}`;
@@ -151,26 +166,46 @@ export const createNewIntegratedHyloGroup = async (req, res) => {
 };
 
 export const inviteMemberToHyloGroup = async (req, res) => {
-  await new Promise((r) => setTimeout(r, 2000));
-  return res.json({ ok: true });
   const schema = Joi.object({
     membershipId: Joi.string().required(),
   });
 
   const { membershipId } = validateOrThrow(schema, req.body);
-
+  // Find membership
   const membership = await db
     .collection('memberships')
     .findOne({ _id: new ObjectId(membershipId) });
   if (!membership) {
-    throw boom.notFound(`Can't find membership with the ID "${membershipId}`);
+    throw boom.notFound(`Can't find membership with the ID "${membershipId}"`);
   }
 
-  // TODO asser group admin
+  // Authorize request
+  const adminId = get(res, 'locals.auth.user._id');
+  const hasAdminRole = await rolesService.hasAdminRole(adminId, membership.group);
+  if (!hasAdminRole) {
+    throw boom.unauthorized();
+  }
 
-  // TODO await upsertHyloUser({ name: user.name, email: user.email });
+  // Find the user
+  const user = membership.user && (await db.collection('users').findOne({ _id: membership.user }));
+  // Throw if user is missing (or the membership is pending)
+  if (!user) {
+    throw boom.notFound(`Membership "${membershipId}" has no user`);
+  }
 
-  // TODO addMember
+  // Find Hylo integration
+  const mapping = await db
+    .collection(COLL_GROUPS_HYLO_MAPPINGS)
+    .findOne({ groupId: membership.group });
+  if (!mapping) {
+    throw boom.notFound(`Can't find Hylo integration for group "${membership.group}"`);
+  }
+
+  // Upsert the users and add it to the hylo group
+  const hyloUser = await upsertHyloUser({ name: user.name, email: user.email });
+
+  await addMember({ hyloUserId: hyloUser.id, hyloGroupId: mapping.hyloGroupId });
+  res.json({ ok: true });
 };
 
 export const setIntegratedHyloGroup = async (req, res) => {
