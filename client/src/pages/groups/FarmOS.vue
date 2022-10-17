88c0f34cb7d7bc9ccfbc915632852aaf53502771
Manuel Di Cerbo <mdc+git@serverbox.ch>
2022-10-17 15:38:53 +0200

    lower max width

--- client/src/pages/groups/FarmOS.vue
+++ client/src/pages/groups/FarmOS.vue
@@ -1,57 +1,25 @@
 <template>
-  <v-container v-if="farmosEnabled">
-    <v-alert
-      v-if="successMessage"
-      class="mt-4"
-      style="cursor: pointer"
-      mode="fade"
-      text
-      type="success"
-      @click="successMessage = null"
-      >{{ successMessage }}</v-alert
-    >
+  <v-container v-if="farmosEnabled" class="max-800">
+    <v-alert v-if="successMessage" class="mt-4" style="cursor: pointer" mode="fade" text type="success"
+      @click="successMessage = null">{{ successMessage }}</v-alert>
     <v-alert v-if="errorMessage" style="cursor: pointer" class="mt-4 cursor-pointer" mode="fade" text type="error">{{
-      errorMessage
+    errorMessage
     }}</v-alert>
 
-    <FarmOSCreateDialog
-      v-model="showCreateDialog"
-      v-if="showCreateDialog"
-      @check-url="checkUrl"
-      @create-instance="createInstance"
-      :viewModel="createViewModel"
-    />
+    <FarmOSCreateDialog v-model="showCreateDialog" v-if="showCreateDialog" @check-url="checkUrl"
+      @create-instance="createInstance" :viewModel="createViewModel" />
 
-    <FarmOSConnectDialog
-      v-model="showConnectDialog"
-      :farmInstances="farmInstances"
-      :allowCreate="allowCreate"
-      @connect="connectFarms"
-      @create="createFarm"
-    />
+    <FarmOSConnectDialog v-model="showConnectDialog" :farmInstances="farmInstances" :allowCreate="allowCreate"
+      @connect="connectFarms" @create="createFarm" />
 
-    <FarmOSDisconnectDialog
-      v-model="showDisonnectDialog"
-      :disconnectFarmInstanceName="disconnectFarmInstanceName"
-      :groupsOfInstance="groupsOfInstance"
-      :disconnectGroupId="disconnectGroupId"
-      @disconnect="disconnectGroups"
-    />
+    <FarmOSDisconnectDialog v-model="showDisonnectDialog" :disconnectFarmInstanceName="disconnectFarmInstanceName"
+      :groupsOfInstance="groupsOfInstance" :disconnectGroupId="disconnectGroupId" @disconnect="disconnectGroups" />
 
-    <FarmOSGroupSettings
-      class="ma-16"
-      @addGrpCoffeeShop="enableCoffeeshop"
+    <FarmOSGroupSettings class="ma-16" @addGrpCoffeeShop="enableCoffeeshop"
       @allowSbGrpsJoinCoffeeShop="allowSubGroupsJoinCoffeeShop"
-      @allowSbGrpsAdminsCreateFarmOSFarms="allowSubGroupsAdminsCreateFarmOSFarms"
-      @connect="connect"
-      @disconnect="showDisconnectGroupsDialog"
-      @open="openFarm"
-      @plansChanged="plansChanged"
-      @seatsChanged="seatsChanged"
-      :plans="plans"
-      :groupInfos="groupInfos"
-      :superAdmin="superAdmin"
-    >
+      @allowSbGrpsAdminsCreateFarmOSFarms="allowSubGroupsAdminsCreateFarmOSFarms" @connect="connect"
+      @disconnect="showDisconnectGroupsDialog" @open="openFarm" @plansChanged="plansChanged"
+      @seatsChanged="seatsChanged" :plans="plans" :groupInfos="groupInfos" :superAdmin="superAdmin">
     </FarmOSGroupSettings>
   </v-container>
 
@@ -67,14 +35,12 @@
           <p>{{ message }}</p>
           <v-btn color="primary" type="submit" @click="enable" v-if="btnEnable">Enable</v-btn>
           <v-btn color="primary" type="submit" href="mailto:info@our-sci.net" target="_blank" v-else-if="btnContact">
-            Contact Our-Sci</v-btn
-          >
+            Contact Our-Sci</v-btn>
         </v-card>
         <v-card class="pa-8 text-center" v-else>
           <p>{{ message }}</p>
-          <v-btn color="primary" type="submit" href="mailto:info@our-sci.net" target="_blank" v-if="btnContact"
-            >Contact Our-Sci</v-btn
-          >
+          <v-btn color="primary" type="submit" href="mailto:info@our-sci.net" target="_blank" v-if="btnContact">Contact
+            Our-Sci</v-btn>
         </v-card>
       </v-col>
     </v-row>
@@ -160,9 +126,9 @@ export default {
     */
   },
   methods: {
-    openFarm() {},
-    updateGroupConfig() {},
-    unifomMembersInGroupInfos() {},
+    openFarm() { },
+    updateGroupConfig() { },
+    unifomMembersInGroupInfos() { },
     async seatsChanged(seats) {
       try {
         await api.post(`/farmos/group-manage/${this.groupId}/seats`, { seats: seats });
@@ -511,3 +477,10 @@ export default {
   },
 };
 </script>
+
+
+<style scoped>
+.max-800 {
+  max-width: 800px;
+}
+</style>
