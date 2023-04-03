73291e3fb59b790772f3747d3f3a49626fcc9407
Benoit De Sousa <benoit.desousa@nexus-computing.ch>
2023-04-03 12:38:22 +0200

    ET timestamp fixed

--- server/src/controllers/farmosController.js
+++ server/src/controllers/farmosController.js
@@ -1178,11 +1178,14 @@ const assertGroupsInTree = async (groupIds, tree) => {
 const getCurrentDateAsString = () => {
   const date = new Date();
   return date.toLocaleString('en-US', {
+    timeZone: 'America/New_York',
     year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZoneName: 'short',
   });
 };
 
