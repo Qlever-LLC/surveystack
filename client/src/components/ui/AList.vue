17cc4b52d9923662ea2b7ae3035869bcaaec8e26
manuel-ch <mhaerdi@gmx.ch>
2023-08-18 16:44:15 +0200

    add v-list wrapper

---
+++ client/src/components/ui/AList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-list :flat="flat" :dense="dense" :two-line="twoLine" :subheader="subheader" :max-width="maxWidth">
+      <slot />
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    flat: { type: Boolean, required: false },
+    dense: { type: Boolean, required: false },
+    twoLine: { type: Boolean, required: false },
+    subheader: { type: Boolean, required: false },
+    maxWidth: { type: [Number, String], required: false },
+  },
+};
+</script>
