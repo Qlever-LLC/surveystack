7207a5e44229340b3bfb22bd83ffddbddc74fe89
manuel-ch <mhaerdi@gmx.ch>
2023-08-21 13:22:03 +0200

    add v-icon wrapper

---
+++ client/src/components/ui/AIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-icon :color="color" :size="size" :left="left">
+    <slot />
+  </v-icon>
+</template>
+
+<script>
+export default {
+  props: {
+    color: { type: Boolean, required: false },
+    left: { type: Boolean, required: false }, //v3: start
+    right: { type: Boolean, required: false }, //v3: end
+    size: { type: [String, Number], required: false }, //number in pixels or one of x-small, small, default, large, and x-large
+    dark: { type: Boolean, required: false },
+    disabled: { type: Boolean, required: false }, //v3: replace by v-btn
+  },
+};
+</script>
+
+<style scoped></style>
