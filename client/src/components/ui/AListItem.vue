5984387fb5139794738aa03e9b35b73f119a8de0
manuel-ch <mhaerdi@gmx.ch>
2023-08-22 16:23:21 +0200

    create wrapper for v-list-item

---
+++ client/src/components/ui/AListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-list-item
+      :flat="flat"
+      :dense="dense"
+      :link="link"
+      :two-line="twoLine"
+      :three-line="threeLine"
+      :href="href"
+      :to="to"
+      :target="target"
+      :value="value"
+    >
+      <slot />
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    flat: { type: Boolean, required: false },
+    dense: { type: Boolean, required: false },
+    link: { type: Boolean, required: false },
+    twoLine: { type: Boolean, required: false },
+    threeLine: { type: Boolean, required: false },
+    href: { type: [String, Object], required: false },
+    to: { type: [String, Object], required: false },
+    target: { type: String, required: false },
+    value: { type: undefined, required: false },
+    //click
+  },
+};
+</script>
