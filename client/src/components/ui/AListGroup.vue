ff3009e28e5fd0d44eac78f1fab8238ea87230c9
manuel-ch <mhaerdi@gmx.ch>
2023-09-04 16:55:11 +0200

    create wrapper for v-list-group

---
+++ client/src/components/ui/AListGroup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-list-group :prepend-icon="prependIcon" :value="value" :color="color">
+      <slot />
+    </v-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    prependIcon: { type: String, required: false },
+    value: { type: undefined, required: false },
+    color: { type: String, required: false },
+  },
+};
+</script>
