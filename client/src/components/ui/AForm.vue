a7140876bd97fc01d724d6e9b003356c18bfff95
manuel-ch <mhaerdi@gmx.ch>
2023-10-24 14:14:48 +0200

    add AForm.vue wrapping VForm

---
+++ client/src/components/ui/AForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-form
+    :model-value="value"
+    :disabled="disabled"
+    :lazy-validation="lazyValidation"
+    :readonly="readOnly"
+    @submit="$emit('submit', $event)"
+    @input="$emit('input', $event)"
+  >
+    <template v-slot:default>
+      <slot name="default" />
+    </template>
+  </v-form>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: { type: Boolean, default: false },
+    lazyValidation: { type: Boolean, default: false },
+    readOnly: { type: Boolean, default: false },
+    value: { type: Boolean, default: false },
+  },
+};
+</script>
