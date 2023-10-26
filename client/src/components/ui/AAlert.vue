97178db48f5c50dcca06fbd5efec945cfc52b56f
manuel-ch <mhaerdi@gmx.ch>
2023-10-26 09:56:45 +0200

    add AAlert wrapping v-alert

---
+++ client/src/components/ui/AAlert.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-alert
+    :border="border"
+    :color="color"
+    :colored-border="coloredBorder"
+    :dismissible="dismissible"
+    :elevation="elevation"
+    :icon="icon"
+    :mode="mode"
+    :outlined="outlined"
+    :prominent="prominent"
+    :text="text"
+    :type="type"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <template v-slot:default>
+      <slot name="default" />
+    </template>
+  </v-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    border: { type: String, required: false },
+    coloredBorder: { type: Boolean, default: false },
+    dismissible: { type: Boolean, default: false },
+    elevation: { type: [Number, String], required: false },
+    icon: { type: [Boolean, String], required: false },
+    mode: { type: String, required: false },
+    outlined: { type: Boolean, default: false },
+    prominent: { type: Boolean, default: false },
+    text: { type: Boolean, default: false },
+    type: { type: String, required: false },
+  },
+};
+</script>
