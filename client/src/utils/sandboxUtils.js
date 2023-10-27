21a95a735087fb932d44a6b16a67161d9bdb98d7
Greg Austic <gbathree@gmail.com>
2023-10-27 17:22:00 +0000

    fixed array optional chaining ?.[ in all 4 locations

--- client/src/utils/sandboxUtils.js
+++ client/src/utils/sandboxUtils.js
@@ -369,7 +369,7 @@ const unstable = {
       let result = await response.text();
       result = JSON.parse(result);
       // if not present, search in non-archived surveys
-      if (result.length === 0 || !result?.[0]?.data) {
+      if (result.length === 0 || !result[0] || !result[0].data) {
         url = `https://app.surveystack.io/api/submissions?survey=${surveyId}&match={"_id":{"$oid":"${submissionId}"}}`;
         this.prettyLog('check submission in non archived url', 'info');
         this.prettyLog(url);
@@ -377,7 +377,7 @@ const unstable = {
         result = await response.text();
         result = JSON.parse(result);
       }
-      if (result?.[0]?.data) {
+      if (result && result[0] && result[0].data) {
         submission = result[0]; // assign this so we can test against it
         this.prettyLog(`found: submission id ${submissionId}`, 'success');
         this.prettyLog(submission);
@@ -420,7 +420,7 @@ const unstable = {
             if (returnColumns && returnColumns.length) {
               let someItems = {};
               returnColumns.forEach((column) => {
-                if (items?.[column]) {
+                if (items && items[column]) {
                   someItems[column] = items[column];
                 } else {
                   this.prettyLog(`Did not find ${column} in ${lookup}`, 'warning');
@@ -441,28 +441,28 @@ const unstable = {
     return items && Object.keys(items).length > 0 ? items : null;
   },
 
- /**
-   * prettyLog
-   *
-   * Logs messages with optional colored backgrounds based on the status parameter.
-   * Logging is controlled by the config.log flag.
-   *
-   * @param {string} label - The message to log.
-   * @param {string} [status=''] - The status of the message, which determines the background color. 
-   * @param {boolean} [show=true] - Optional parameter to print.  Use as switch to show all in a script / document 
-   * Valid values are 'success', 'warning', 'info', or an empty string for the default console.log style.
-   */
- prettyLog = (label, status = '', show = true) => {
-  if (show) {
+  /**
+    * prettyLog
+    *
+    * Logs messages with optional colored backgrounds based on the status parameter.
+    * Logging is controlled by the config.log flag.
+    *
+    * @param {string} label - The message to log.
+    * @param {string} [status=''] - The status of the message, which determines the background color. 
+    * @param {boolean} [show=true] - Optional parameter to print.  Use as switch to show all in a script / document 
+    * Valid values are 'success', 'warning', 'info', or an empty string for the default console.log style.
+    */
+  prettyLog = (label, status = '', show = true) => {
+    if (show) {
       const styles = {
         success: 'background-color: #49d65e; padding: 0.2rem 1.5rem;',
         warning: 'background-color: #de9250; padding: 0.2rem 1.5rem;',
         info: 'background-color: #d9de45; padding: 0.2rem 1.5rem;',
       };
       const style = styles[status] || '';
-      console.log('%c' + label, style);  
-  }
-},
+      console.log('%c' + label, style);
+    }
+  },
 
   /*
    * remove the survey root and return the text
@@ -649,8 +649,14 @@ const unstable = {
    * @returns {Object} - The updated object (log, asset, etc.)
    */
   addRelationshipData(obj, relationship, type, field, ...names) {
-    if (!obj.entity?.relationships) obj.entity.relationships = {}; // check that this exists, make exist is not
-    if (!obj.entity?.relationships?.[relationship]) obj.entity.relationships[relationship] = { data: [] }; // same
+    if (obj.entity) {
+      if (!obj.entity.relationships) {
+        obj.entity.relationships = {};
+      }
+      if (!obj.entity.relationships[relationship]) {
+        obj.entity.relationships[relationship] = { data: [] };
+      }
+    }
     if (obj && relationship && type && names) {
       names.forEach((name) => {
         const isDuplicate = obj.entity.relationships[relationship].data.some((obj) => obj[field] === name);
