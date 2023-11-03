a75f875f4df448619f9aa64fb32d87bdf103604a
manuel-ch <mhaerdi@gmx.ch>
2023-11-03 09:39:56 +0100

    create AMenu.vue wrapping v-menu

---
+++ client/src/components/ui/AMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-menu
+    :disabled="disabled"
+    :attach="attach"
+    :max-height="maxHeight"
+    :max-width="maxWidth"
+    :min-width="minWidth"
+    :transition="transition"
+    :close-on-content-click="closeOnContentClick"
+    :left="left"
+    :offset-y="offsetY"
+    :value="value"
+    v-bind="$attrs"
+    @change="$emit('input', $event)"
+  >
+    <slot />
+  </v-menu>
+</template>
+
+<script>
+export default {
+  props: {
+    attach: {},
+    maxHeight: {},
+    maxWidth: {},
+    minWidth: {},
+    closeOnContentClick: {},
+    left: {},
+    transition: {},
+    disabled: { type: Boolean, default: false },
+    offsetY: { type: Boolean, default: false },
+    value: { type: undefined, required: false },
+  },
+  emits: ['input'],
+};
+</script>
+
+<style scoped></style>
