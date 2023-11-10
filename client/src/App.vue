1b780091a8de580a97cb7c83db4c4c5939d051d1
Benoit De Sousa <benoit.desousa@nexus-computing.ch>
2023-11-10 09:51:52 +0100

    create AMain.vue wrapping v-main

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,10 +2,10 @@
   <v-app id="app" :class="{ 'minimal-ui': $route.query.minimal_ui }">
     <router-view name="navbar" />
     <div id="app-menu"></div>
-    <v-main>
+    <a-main>
       <app-global-feedback />
       <router-view />
-    </v-main>
+    </a-main>
     <install-banner />
   </v-app>
 </template>
@@ -16,12 +16,14 @@ import domainHandler from '@/utils/domainHandler';
 import api from '@/services/api.service';
 import * as db from '@/store/db';
 import InstallBanner from '@/components/ui/InstallBanner.vue';
+import AMain from '@/components/ui/AMain.vue';
 
 export default {
   name: 'App',
   components: {
     appGlobalFeedback,
     InstallBanner,
+    AMain,
   },
   created() {
     domainHandler.install(this);
