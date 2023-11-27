60c3401396f367e691ad50912afae3dfa08a25e4
Benoit De Sousa <benoit.desousa@nexus-computing.ch>
2023-11-27 17:48:39 +0100

    fix eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier', 'plugin:storybook/recommended'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier', 'plugin:storybook/recommended'],
   plugins: ['import'],
   ignorePatterns: ['marked.esm.js'],
   rules: {
