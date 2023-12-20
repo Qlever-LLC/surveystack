83234185df259269ccced825fa72d917e48cff1d
manuel-ch <mhaerdi@gmx.ch>
2023-12-20 12:29:13 +0100

    fix transformer version

--- client/jest.config.js
+++ client/jest.config.js
@@ -27,6 +27,6 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'vue'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '^.+\\.vue$': '@vue/vue2-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
   },
 };
