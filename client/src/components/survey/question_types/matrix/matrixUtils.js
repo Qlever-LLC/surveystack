3a035bef82e9a77b34c06414dd2855dbd57b59da
Will Gardiner <gardiner.w@gmail.com>
2024-01-11 16:20:44 -0500

    fix createRow in matrixUtils

--- client/src/components/survey/question_types/matrix/matrixUtils.js
+++ client/src/components/survey/question_types/matrix/matrixUtils.js
@@ -3,7 +3,7 @@ export const createRow = (fields, headers) => {
   const newRow = Object.fromEntries(
     fields.map((field) => {
       const header = headers.find((h) => h.value === field);
-      const value = header ? header.defaultValue : null;
+      const value = (header && header.defaultValue) || null;
       const meta = header && header.redacted ? { permissions: ['admin'] } : undefined;
       return [field, { value, meta }];
     })
