fa1edfd0e342baded2d727c308f17d4a4d06ea9d
Benoit De Sousa <benoit.desousa@nexus-computing.ch>
2024-01-22 21:09:31 +0100

    upgrade AForm used in MembershipNew and switch to script setup with router, route and store

--- client/src/pages/memberships/MembershipNew.vue
+++ client/src/pages/memberships/MembershipNew.vue
@@ -1,7 +1,7 @@
 <template>
   <a-container>
-    <span class="text-secondary overline">{{ entity._id }}</span>
-    <h2>Invite people to '{{ groupDetail.name }}'</h2>
+    <span class="text-secondary overline">{{ state.entity._id }}</span>
+    <h2>Invite people to '{{ state.groupDetail.name }}'</h2>
     <a-card class="pa-4 mb-4">
       <a-form ref="form" class="mt-3" @keydown.enter.prevent="submit">
         <a-select
@@ -9,22 +9,22 @@
           :items="availableRoles"
           item-title="text"
           item-value="value"
-          v-model="entity.role"
+          v-model="state.entity.role"
           label="Role"
           variant="outlined" />
 
         <a-text-field
           class="mt-3"
-          v-model="entity.meta.invitationEmail"
+          v-model="state.entity.meta.invitationEmail"
           label="Email"
           variant="outlined"
-          :rules="emailRules"
+          :rules="state.emailRules"
           validate-on-blur
           hint="Choose an email address you will not lose access to.  Changing an email address later may cause some integrations to not work." />
 
         <a-text-field
           class="mt-3"
-          v-model="entity.meta.invitationName"
+          v-model="state.entity.meta.invitationName"
           variant="outlined"
           hint="Default name for newly registered users"
           labelSlot>
@@ -33,7 +33,10 @@
           </template>
         </a-text-field>
 
-        <a-radio-group v-model="sendEmail" name="sendEmail" :disabled="invitationMethod === INVITATION_METHODS.ADD">
+        <a-radio-group
+          v-model="state.sendEmail"
+          name="sendEmail"
+          :disabled="state.invitationMethod === INVITATION_METHODS.ADD">
           <a-radio label="Send an invitation email" value="SEND_NOW" labelSlot>
             <template v-slot:label>
               <div>
@@ -56,16 +59,16 @@
           <a-btn variant="text" @click="cancel">Cancel</a-btn>
 
           <btn-dropdown
-            :label="invitationMethod === INVITATION_METHODS.INVITE ? 'Invite Member' : 'Add Member'"
+            :label="state.invitationMethod === INVITATION_METHODS.INVITE ? 'Invite Member' : 'Add Member'"
             :show-drop-down="true"
-            :disabled="!submittable"
-            :loading="isSubmitting"
+            :disabled="!state.submittable"
+            :loading="state.isSubmitting"
             @click="submit"
             color="primary"
             elevation="0"
             top
             left>
-            <a-list class="pa-0 mx-auto" max-width="280" v-model:selected="invitationMethod">
+            <a-list class="pa-0 mx-auto" max-width="280" v-model:selected="state.invitationMethod">
               <a-list-item two-line :value="INVITATION_METHODS.INVITE">
                 <a-list-item-title>Invite Member</a-list-item-title>
                 <div class="multiline-subtitle">
@@ -88,15 +91,15 @@
       </a-form>
     </a-card>
 
-    <a-dialog v-model="dialogCreateUser" max-width="500">
+    <a-dialog v-model="state.dialogCreateUser" max-width="500">
       <a-card>
         <a-card-title> User does not exist yet </a-card-title>
         <a-card-text class="mt-4">
-          Do you want to proceed to create a new user with email {{ this.entity.meta.invitationEmail }}
+          Do you want to proceed to create a new user with email {{ state.entity.meta.invitationEmail }}
         </a-card-text>
         <a-card-actions>
           <a-spacer />
-          <a-btn variant="text" @click.stop="dialogCreateUser = false"> Cancel </a-btn>
+          <a-btn variant="text" @click.stop="state.dialogCreateUser = false"> Cancel </a-btn>
           <a-btn variant="text" color="red" @click.stop="proceedToUserCreation"> Proceed </a-btn>
         </a-card-actions>
       </a-card>
@@ -104,16 +107,27 @@
   </a-container>
 </template>
 
-<script>
+<script setup>
 import ObjectId from 'bson-objectid';
 import api from '@/services/api.service';
 import EmailValidator from 'email-validator';
 
 import { uuid } from '@/utils/memberships';
 import BtnDropdown from '@/components/ui/BtnDropdown';
+import { ref, reactive, watch, onMounted } from 'vue';
+import { useStore } from 'vuex';
+import { useRouter, useRoute } from 'vue-router';
+
+const store = useStore();
+const router = useRouter();
+const route = useRoute();
 
 // LocalStorage key for saving the preferred login method
 const LS_MEMBER_INVITATION_METHOD = 'last-used-invitation-method-on-new-member-page';
+const INVITATION_METHODS = {
+  INVITE: 'invite',
+  ADD: 'add',
+};
 
 const availableRoles = [
   {
@@ -126,112 +140,98 @@ const availableRoles = [
   },
 ];
 
-export default {
-  components: { BtnDropdown },
-  data() {
-    const INVITATION_METHODS = {
-      INVITE: 'invite',
-      ADD: 'add',
-    };
-    return {
-      availableRoles,
-      entity: {
-        _id: '',
-        user: null,
-        group: null,
-        role: 'user',
-        meta: {
-          status: 'pending',
-          dateCreated: new Date().toISOString(),
-          dateSent: null,
-          dateActivated: null,
-          notes: '',
-          invitationEmail: null,
-          invitationName: null,
-          invitationCode: uuid(),
-        },
-      },
-      groupDetail: { name: '', path: '' },
-      dialogCreateUser: false,
-      sendEmail: 'SEND_NOW',
-      INVITATION_METHODS,
-      invitationMethod: Object.values(INVITATION_METHODS).includes(localStorage[LS_MEMBER_INVITATION_METHOD])
-        ? [localStorage[LS_MEMBER_INVITATION_METHOD]]
-        : [INVITATION_METHODS.INVITE],
-      isSubmitting: false,
-      emailRules: [(v) => !!v || 'E-mail is required', (v) => EmailValidator.validate(v) || 'E-mail must be valid'],
-    };
+const state = reactive({
+  entity: {
+    _id: '',
+    user: null,
+    group: null,
+    role: 'user',
+    meta: {
+      status: 'pending',
+      dateCreated: new Date().toISOString(),
+      dateSent: null,
+      dateActivated: null,
+      notes: '',
+      invitationEmail: null,
+      invitationName: null,
+      invitationCode: uuid(),
+    },
   },
-  methods: {
-    cancel() {
-      this.$router.back();
-    },
-    updateCode(code) {
-      this.entity.content = code;
-    },
-    async submit() {
-      if (!this.$refs.form.validate()) {
-        return;
-      }
-      const data = this.entity;
-      const url =
-        this.invitationMethod === [this.INVITATION_METHODS.INVITE]
-          ? `/memberships?sendEmail=${this.sendEmail}`
-          : `/memberships/confirmed`;
+  groupDetail: { name: '', path: '' },
+  dialogCreateUser: false,
+  sendEmail: 'SEND_NOW',
+  invitationMethod: Object.values(INVITATION_METHODS).includes(localStorage[LS_MEMBER_INVITATION_METHOD])
+    ? [localStorage[LS_MEMBER_INVITATION_METHOD]]
+    : [INVITATION_METHODS.INVITE],
+  isSubmitting: false,
+  emailRules: [(v) => !!v || 'E-mail is required', (v) => EmailValidator.validate(v) || 'E-mail must be valid'],
+  submittable: true,
+});
 
-      try {
-        this.isSubmitting = true;
-        await api.post(url, data);
-        this.$router.back();
-      } catch (err) {
-        if (err.response.status === 404) {
-          // legacy (no 404 will be thrown from the server)
-          this.dialogCreateUser = true;
-        }
-        this.$store.dispatch('feedback/add', err.response.data.message);
-      } finally {
-        this.isSubmitting = false;
-      }
-    },
-    proceedToUserCreation() {
-      this.$router.replace({
-        name: 'users-new',
-        query: {
-          group: this.entity.group,
-          role: this.entity.role,
-          email: this.entity.meta.invitationEmail,
-        },
-      });
-    },
-  },
-  computed: {
-    submittable() {
-      return true;
-    },
-  },
-  watch: {
-    invitationMethod(newValue) {
-      localStorage[LS_MEMBER_INVITATION_METHOD] = newValue[0];
-    },
-  },
-  async created() {
-    this.entity._id = new ObjectId();
+const form = ref(null);
 
-    const { group, role } = this.$route.query;
-    if (!group || !role) {
-      return;
+function cancel() {
+  router.back();
+}
+function updateCode(code) {
+  state.entity.content = code;
+}
+async function submit() {
+  if (!(await form.value.validate())) {
+    return;
+  }
+  const data = state.entity;
+  const url =
+    state.invitationMethod[0] === INVITATION_METHODS.INVITE
+      ? `/memberships?sendEmail=${state.sendEmail}`
+      : `/memberships/confirmed`;
+
+  try {
+    state.isSubmitting = true;
+    await api.post(url, data);
+    router.back();
+  } catch (err) {
+    if (err.response.status === 404) {
+      // legacy (no 404 will be thrown from the server)
+      state.dialogCreateUser = true;
     }
+    await store.dispatch('feedback/add', err.response.data.message);
+  } finally {
+    state.isSubmitting = false;
+  }
+}
+function proceedToUserCreation() {
+  router.replace({
+    name: 'users-new',
+    query: {
+      group: state.entity.group,
+      role: state.entity.role,
+      email: state.entity.meta.invitationEmail,
+    },
+  });
+}
 
-    try {
-      const { data: groupDetail } = await api.get(`/groups/${group}`);
-      this.groupDetail = groupDetail;
-      this.entity.group = group;
-      this.entity.role = role;
-    } catch (e) {
-      console.log('something went wrong:', e);
-    }
-  },
-};
+watch(state.invitationMethod, (newValue) => {
+  localStorage[LS_MEMBER_INVITATION_METHOD] = newValue[0];
+});
+
+onMounted(async () => {
+  state.entity._id = new ObjectId();
+
+  const { group, role } = route.query;
+  if (!group || !role) {
+    return;
+  }
+
+  try {
+    const { data: groupDetailData } = await api.get(`/groups/${group}`);
+    state.groupDetail = groupDetailData;
+    state.entity.group = group;
+    state.entity.role = role;
+  } catch (e) {
+    console.log('something went wrong:', e);
+  }
+});
 </script>
 
 <style scoped lang="scss">
