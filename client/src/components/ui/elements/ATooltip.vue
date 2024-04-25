724013d70d5c622e5e86c2874cd4283a265e29e2
Benoit De Sousa <benoit.desousa@nexus-computing.ch>
2024-04-25 13:42:01 +0200

    update and migrate to script setup ATooltip

--- client/src/components/ui/elements/ATooltip.vue
+++ client/src/components/ui/elements/ATooltip.vue
@@ -1,14 +1,10 @@
 <template>
   <v-tooltip
-    :bottom="bottom"
     :disabled="disabled"
-    :left="left"
+    :location="getLocation()"
     :max-width="maxWidth"
     :open-delay="openDelay"
-    :right="right"
-    :top="top"
-    :transition="transition"
-  >
+    :transition="transition">
     <template v-slot:activator="{ props }">
       <slot name="activator" :props="props"></slot>
     </template>
@@ -16,18 +12,37 @@
   </v-tooltip>
 </template>
 
-<script>
-export default {
-  props: {
-    // //vuetify props
-    bottom: { type: Boolean, required: false },
-    disabled: { type: Boolean, required: false },
-    left: { type: Boolean, required: false },
-    maxWidth: { type: [Number, String], required: false },
-    openDelay: { type: [Number, String], required: false },
-    right: { type: Boolean, required: false },
-    top: { type: Boolean, required: false },
-    transition: { type: String, required: false },
-  },
-};
+<script setup>
+import { reactive } from 'vue';
+
+const props = defineProps({
+  // //vuetify props
+  bottom: { type: Boolean, required: false },
+  disabled: { type: Boolean, required: false },
+  left: { type: Boolean, required: false },
+  maxWidth: { type: [Number, String], required: false },
+  openDelay: { type: [Number, String], required: false },
+  right: { type: Boolean, required: false },
+  top: { type: Boolean, required: false },
+  transition: { type: String, required: false },
+});
+
+const state = reactive({
+  location: undefined,
+});
+
+function getLocation() {
+  if (props.bottom) {
+    return (state.location = 'bottom');
+  }
+  if (props.left) {
+    return (state.location = 'start');
+  }
+  if (props.right) {
+    return (state.location = 'end');
+  }
+  if (props.top) {
+    return (state.location = 'top');
+  }
+}
 </script>
