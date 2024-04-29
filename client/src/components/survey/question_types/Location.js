2b5879872224cbb512d3545b0643435324cfa1e2
manuel-ch <mhaerdi@gmx.ch>
2024-04-29 14:00:43 +0200

    - refactor location question - add questionComponent.js composable to be used as in composition api in place of the BaseQuestionComponent.js mixin

--- client/src/components/survey/question_types/Location.js
+++ client/src/components/survey/question_types/Location.js
@@ -1,21 +1,4 @@
-import mapboxgl from 'mapbox-gl';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-
-import AppGps from '@/components/ui/Gps.vue';
-import baseQuestionComponent from './BaseQuestionComponent';
-import { isOnline } from '@/utils/surveyStack';
-
-const requestWakeLock = async () => {
-  try {
-    await navigator.wakeLock.request('screen');
-  } catch (e) {
-    console.error(`${e.name}, ${e.message}`);
-  }
-};
-
-const MAP_STYLES = ['satellite', 'streets', 'outdoors'];
-
-const geoJsonFromLngLat = ({ lng, lat }) => ({
+export const geoJsonFromLngLat = ({ lng, lat }) => ({
   type: 'Feature',
   geometry: {
     type: 'Point',
@@ -26,7 +9,7 @@ const geoJsonFromLngLat = ({ lng, lat }) => ({
   },
 });
 
-const geoJsonFromPosition = ({ coords }) => ({
+export const geoJsonFromPosition = ({ coords }) => ({
   type: 'Feature',
   geometry: {
     type: 'Point',
@@ -37,208 +20,12 @@ const geoJsonFromPosition = ({ coords }) => ({
   },
 });
 
-export default {
-  mixins: [baseQuestionComponent],
-  components: {
-    AppGps,
-  },
-  data() {
-    return {
-      usingGPS: true,
-      panel: [0],
-      map: null,
-      mapStyle: MAP_STYLES[0],
-      marker: null,
-      ctrl: null,
-      mapError: false,
-      offlineMode: false,
-      location: null,
-      gpsLocation: null,
-      gpsTimer: 0,
-      geolocationError: false,
-    };
-  },
-  methods: {
-    pickLocation() {
-      this.location = this.usingGPS ? this.gpsLocation : geoJsonFromLngLat(this.map.getCenter());
-      this.changed(this.location);
-
-      if (!this.location || !this.map) {
-        return;
-      }
-
-      if (this.marker) {
-        this.marker.remove();
-        this.marker = null;
-      }
-
-      this.marker = new mapboxgl.Marker().setLngLat(this.location.geometry.coordinates).addTo(this.map);
-    },
-    reset() {
-      this.changed(null);
-      if (this.marker) {
-        this.marker.remove();
-        this.marker = null;
-      }
-      this.location = null;
-      this.startGpsTimer();
-    },
-    switchMapStyle() {
-      const index = MAP_STYLES.indexOf(this.mapStyle);
-      const newIndex = (index + 1) % MAP_STYLES.length;
-      this.mapStyle = MAP_STYLES[newIndex];
-      this.map.setStyle(`mapbox://styles/mapbox/${this.mapStyle}-v9`);
-    },
-    startGpsTimer() {
-      this.ctrl.trigger();
-      this.stopGpsTimer();
-      this.gpsTimer = setTimeout(() => {
-        console.warn('No confirmation from the user permission');
-        this.geolocationError = 'No confirmation from the user permission';
-        this.usingGPS = false;
-      }, 30000);
-    },
-    stopGpsTimer() {
-      if (!this.gpsTimer) {
-        return;
-      }
-
-      clearTimeout(this.gpsTimer);
-      this.gpsTimer = 0;
-    },
-    startMap() {
-      setTimeout(() => {}, 2000);
-      mapboxgl.accessToken = 'pk.eyJ1Ijoib3Vyc2NpIiwiYSI6ImNqb2ljdHMxYjA1bDAzcW03Zjd0cHBsbXMifQ.rL9QPLvi0kLP3DzLt1PQBA';
-      this.map = new mapboxgl.Map({
-        container: `map-question-${this.index}`,
-        style: `mapbox://styles/mapbox/${this.mapStyle}-v9`,
-        zoom: 15,
-      });
-
-      const geocoder = new MapboxGeocoder({
-        // Initialize the geocoder
-        accessToken: mapboxgl.accessToken, // Set the access token
-        mapboxgl: this.map, // Set the mapbox-gl instance
-        marker: false, // Do not use the default marker style
-      });
-
-      geocoder.on('result', () => {
-        this.usingGPS = false;
-      });
-
-      document.getElementById(`map-question-geocoder-${this.index}`).appendChild(geocoder.onAdd(this.map));
-
-      this.ctrl = new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-      });
-
-      this.ctrl.on('trackuserlocationstart', () => {
-        this.usingGPS = true;
-      });
-
-      this.ctrl.on('geolocate', (position) => {
-        this.stopGpsTimer();
-        this.gpsLocation = geoJsonFromPosition(position);
-      });
-
-      this.ctrl.on('error', (e) => {
-        console.warn(e);
-        this.geolocationError = e;
-        this.usingGPS = false;
-        this.stopGpsTimer();
-      });
-
-      this.map.addControl(this.ctrl);
-
-      if (this.modelValue) {
-        this.marker = new mapboxgl.Marker().setLngLat(this.modelValue.geometry.coordinates).addTo(this.map);
-
-        this.map.jumpTo({
-          center: this.modelValue.geometry.coordinates,
-        });
-      }
-
-      this.map.on('load', () => {
-        if (!this.modelValue) {
-          this.startGpsTimer();
-        }
-        this.map.resize();
-      });
-
-      this.map.on('error', (err) => {
-        console.log(err);
-        this.mapError = true;
-        this.usingGPS = true;
-        if (!isOnline()) {
-          this.loadCurrentPositionOffline();
-        }
-      });
-
-      this.map.on('drag', () => {
-        this.usingGPS = false;
-      });
-    },
-    loadCurrentPositionOffline() {
-      if (navigator.geolocation) {
-        this.offlineMode = true;
-        navigator.geolocation.getCurrentPosition((position) => {
-          this.gpsLocation = geoJsonFromPosition(position);
-          this.changed(this.gpsLocation);
-        });
-      }
-    },
-  },
-  computed: {
-    currentLocation() {
-      if (this.modelValue) {
-        return {
-          location: this.modelValue,
-          // label: `used ${this.modelValue.acc ? ' GPS' : 'Map Center'}`,
-          label: `${this.modelValue.properties ? 'Saved Location' : 'Current Map Center'}`,
-        };
-      }
-
-      return {
-        location: this.usingGPS ? this.gpsLocation : geoJsonFromLngLat(this.map.getCenter()),
-        label: this.usingGPS ? 'Using GPS' : 'Using Map Center',
-      };
-    },
-    disablePick() {
-      return this.geolocationError && this.mapError;
-    },
-  },
-  watch: {
-    gpsLocation: {
-      handler(val) {
-        if (val) {
-          this.stopGpsTimer();
-        }
-
-        if (!this.map || !this.usingGPS) {
-          return;
-        }
-
-        this.map.stop();
-        this.map.jumpTo({ center: val.geometry.coordinates });
-      },
-      deep: true,
-    },
-  },
-  created() {
-    this.location = this.modelValue;
-  },
-  mounted() {
-    this.startMap();
-
+export const requestWakeLock = async () => {
+  try {
     if (navigator.wakeLock) {
-      requestWakeLock();
+      await navigator.wakeLock.request('screen');
     }
-  },
-  beforeUnmount() {
-    this.stopGpsTimer();
-    this.map.remove();
-  },
+  } catch (e) {
+    console.error(`${e.name}, ${e.message}`);
+  }
 };
--- client/src/components/survey/question_types/Location.vue
+++ client/src/components/survey/question_types/Location.vue
@@ -9,9 +9,9 @@
       @initialize="initialize" />
     <app-control-hint :value="control.hint" />
 
-    <div v-show="!offlineMode" :id="`map-question-geocoder-${index}`" class="geocoder" style="z-index: 200" />
+    <div v-show="!state.offlineMode" :id="`map-question-geocoder-${index}`" class="geocoder" style="z-index: 200" />
 
-    <div class="map-container my-2" v-if="!mapError">
+    <div class="map-container my-2" v-if="!state.mapError">
       <app-gps class="gps-info" :expanded="false" :location="currentLocation.location">
         {{ currentLocation.label }}
       </app-gps>
@@ -20,20 +20,20 @@
         <a-icon>mdi-layers-outline</a-icon>
       </a-btn>
 
-      <div style="background-color: #000" :id="`map-question-${index}`" class="map-question" v-if="!mapError">
+      <div style="background-color: #000" :id="`map-question-${index}`" class="map-question" v-if="!state.mapError">
         <img v-if="!modelValue" id="map-marker" :src="require('@/assets/marker.svg')" alt="marker" />
         <div class="selection-controls d-flex justify-center">
-          <div v-if="!location">
+          <div v-if="!state.location">
             <a-btn large :disabled="disablePick" class="mx-4 full" color="primary" @click="pickLocation"> Pick </a-btn>
           </div>
           <div v-else>
-            <a-btn large class="mx-4 full" color="gray" @click="reset"> Reset </a-btn>
+            <a-btn large class="mx-4 full" color="gray" @click="reset"> Reset</a-btn>
           </div>
         </div>
       </div>
     </div>
 
-    <div id="map-error-alert" class="my-4" v-else-if="offlineMode">
+    <div id="map-error-alert" class="my-4" v-else-if="state.offlineMode">
       <a-alert type="info" border="end" prominent>
         <b>Offline-Mode</b>: Map unavailable, but position can be determined if permission is granted
       </a-alert>
@@ -56,13 +56,16 @@
     </div>
 
     <div id="map-error-alert" class="my-4" v-else>
-      <a-alert type="info" border="end" prominent> Error loading map. </a-alert>
+      <a-alert type="info" border="end" prominent> Error loading map.</a-alert>
       <app-gps :expanded="true" :location="currentLocation.location">
         {{ currentLocation.label }}
       </app-gps>
     </div>
 
-    <a-overlay class="text-center" :modelValue="!modelValue && !gpsLocation && !mapError && !geolocationError" absolute>
+    <a-overlay
+      class="text-center"
+      :modelValue="!modelValue && !state.gpsLocation && !state.mapError && !state.geolocationError"
+      absolute>
       <a-card>
         <a-card-text>
           <div class="subtitle-1 text-center">Getting GPS Coordinates</div>
@@ -76,6 +79,225 @@
   </div>
 </template>
 
-<script src="./Location.js"></script>
+<script setup>
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+import AppGps from '@/components/ui/Gps.vue';
+import { isOnline } from '@/utils/surveyStack';
+import { geoJsonFromLngLat, geoJsonFromPosition, requestWakeLock } from '@/components/survey/question_types/Location';
+import {
+  QuestionComponentEmits,
+  QuestionComponentProps,
+  useQuestionComponent,
+} from '@/components/survey/question_types/questionComponent';
+import { computed, nextTick, onBeforeUnmount, onMounted, reactive, watch } from 'vue';
+
+const MAP_STYLES = ['satellite', 'streets', 'outdoors'];
+
+const props = defineProps(QuestionComponentProps);
+const emit = defineEmits(QuestionComponentEmits);
+
+const { changed, initialize } = useQuestionComponent(emit);
+
+const state = reactive({
+  location: props.modelValue,
+  usingGPS: true,
+  panel: [0],
+  map: null,
+  mapStyle: MAP_STYLES[0],
+  marker: null,
+  ctrl: null,
+  mapError: false,
+  offlineMode: false,
+  gpsLocation: null,
+  gpsTimer: 0,
+  geolocationError: false,
+});
+
+function pickLocation() {
+  state.location = state.usingGPS ? state.gpsLocation : geoJsonFromLngLat(state.map.getCenter());
+  changed(state.location);
+
+  if (!state.location || !state.map) {
+    return;
+  }
+
+  if (state.marker) {
+    state.marker.remove();
+    state.marker = null;
+  }
+
+  state.marker = new mapboxgl.Marker().setLngLat(state.location.geometry.coordinates).addTo(state.map);
+}
+
+function reset() {
+  changed(null);
+  if (state.marker) {
+    state.marker.remove();
+    state.marker = null;
+  }
+  state.location = null;
+  startGpsTimer();
+}
+
+function switchMapStyle() {
+  const index = MAP_STYLES.indexOf(state.mapStyle);
+  const newIndex = (index + 1) % MAP_STYLES.length;
+  state.mapStyle = MAP_STYLES[newIndex];
+  state.map.setStyle(`mapbox://styles/mapbox/${state.mapStyle}-v9`);
+}
+
+function startGpsTimer() {
+  state.ctrl.trigger();
+  stopGpsTimer();
+  state.gpsTimer = setTimeout(() => {
+    console.warn('No confirmation from the user permission');
+    state.geolocationError = 'No confirmation from the user permission';
+    state.usingGPS = false;
+  }, 30000);
+}
+
+function stopGpsTimer() {
+  if (!state.gpsTimer) {
+    return;
+  }
+
+  clearTimeout(state.gpsTimer);
+  state.gpsTimer = 0;
+}
+
+function startMap() {
+  nextTick();
+  mapboxgl.accessToken = 'pk.eyJ1Ijoib3Vyc2NpIiwiYSI6ImNqb2ljdHMxYjA1bDAzcW03Zjd0cHBsbXMifQ.rL9QPLvi0kLP3DzLt1PQBA';
+  state.map = new mapboxgl.Map({
+    container: `map-question-${props.index}`,
+    style: `mapbox://styles/mapbox/${state.mapStyle}-v9`,
+    zoom: 15,
+  });
+
+  const geocoder = new MapboxGeocoder({
+    // Initialize the geocoder
+    accessToken: mapboxgl.accessToken, // Set the access token
+    mapboxgl: state.map, // Set the mapbox-gl instance
+    marker: false, // Do not use the default marker style
+  });
+
+  geocoder.on('result', () => {
+    state.usingGPS = false;
+  });
+
+  document.getElementById(`map-question-geocoder-${props.index}`).appendChild(geocoder.onAdd(state.map));
+
+  state.ctrl = new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    trackUserLocation: true,
+  });
+
+  state.ctrl.on('trackuserlocationstart', () => {
+    state.usingGPS = true;
+  });
+
+  state.ctrl.on('geolocate', (position) => {
+    stopGpsTimer();
+    state.gpsLocation = geoJsonFromPosition(position);
+  });
+
+  state.ctrl.on('error', (e) => {
+    console.warn(e);
+    state.geolocationError = e;
+    state.usingGPS = false;
+    stopGpsTimer();
+  });
+
+  state.map.addControl(state.ctrl);
+
+  if (props.modelValue) {
+    state.marker = new mapboxgl.Marker().setLngLat(props.modelValue.geometry.coordinates).addTo(state.map);
+
+    state.map.jumpTo({
+      center: props.modelValue.geometry.coordinates,
+    });
+  }
+
+  state.map.on('load', () => {
+    if (!props.modelValue) {
+      startGpsTimer();
+    }
+    state.map.resize();
+  });
+
+  state.map.on('error', (err) => {
+    console.log(err);
+    state.mapError = true;
+    state.usingGPS = true;
+    if (!isOnline()) {
+      loadCurrentPositionOffline();
+    }
+  });
+
+  state.map.on('drag', () => {
+    state.usingGPS = false;
+  });
+}
+
+function loadCurrentPositionOffline() {
+  if (navigator.geolocation) {
+    state.offlineMode = true;
+    navigator.geolocation.getCurrentPosition((position) => {
+      state.gpsLocation = geoJsonFromPosition(position);
+      changed(state.gpsLocation);
+    });
+  }
+}
+
+const currentLocation = computed(() => {
+  if (props.modelValue) {
+    return {
+      location: props.modelValue,
+      // label: `used ${props.modelValue.acc ? ' GPS' : 'Map Center'}`,
+      label: `${props.modelValue.properties ? 'Saved Location' : 'Current Map Center'}`,
+    };
+  }
+
+  return {
+    location: state.usingGPS ? state.gpsLocation : geoJsonFromLngLat(state.map.getCenter()),
+    label: state.usingGPS ? 'Using GPS' : 'Using Map Center',
+  };
+});
+
+const disablePick = computed(() => {
+  return state.geolocationError && state.mapError;
+});
+
+watch(
+  () => state.gpsLocation,
+  async (value, prevValue) => {
+    if (value) {
+      stopGpsTimer();
+    }
+
+    if (!state.map || !state.usingGPS) {
+      return;
+    }
+
+    state.map.stop();
+    state.map.jumpTo({ center: value.geometry.coordinates });
+  },
+  { deep: true }
+);
+
+onMounted(() => {
+  startMap();
+  requestWakeLock();
+});
+
+onBeforeUnmount(() => {
+  stopGpsTimer();
+  state.map.remove();
+});
+</script>
 
 <style scoped src="./Location.css"></style>
