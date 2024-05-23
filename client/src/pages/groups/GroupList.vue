52bf4bca092ba5522e096d3e995a689405b22cd8
manuel-ch <mhaerdi@gmx.ch>
2024-05-23 15:17:13 +0200

    - adjust title and icon based on all/my route - refactor to composition api

--- client/src/pages/groups/GroupList.vue
+++ client/src/pages/groups/GroupList.vue
@@ -2,85 +2,79 @@
   <a-container>
     <basic-list
       listType="card"
-      :entities="entities"
-      :loading="loading"
+      :entities="state.entities"
+      :loading="state.loading"
       groupStyle
       :buttonNew="{ title: 'Create a Group', link: { name: 'groups-new', query: { dir: rootDir } } }"
       :menu="[{ title: 'Go to Group', icon: 'mdi-open-in-new', action: (e) => `/groups/${e._id}`, color: 'green' }]">
       <template v-slot:title>
-        <a-icon class="mr-2"> mdi-compass-outline </a-icon>
-        Find a group
-        <a-chip class="ml-4" color="accent" rounded="lg" variant="flat" disabled> {{ entities.length }} </a-chip>
+        <template v-if="onlyMyGroups()"><a-icon class="mr-2 mt-n1">mdi-account-group</a-icon>All my groups</template>
+        <template v-else> <a-icon class="mr-2">mdi-compass-outline</a-icon>Find a group</template>
+        <a-chip class="ml-4" color="accent" rounded="lg" variant="flat" disabled> {{ state.entities.length }} </a-chip>
       </template>
       <template v-slot:filter>
-        <a-checkbox v-model="showArchived" label="View archived" dense hide-details color="primary" />
+        <a-checkbox v-model="state.showArchived" label="View archived" dense hide-details color="primary" />
       </template>
       <template v-slot:noValue> No Groups available </template>
     </basic-list>
   </a-container>
 </template>
 
-<script>
+<script setup>
 import api from '@/services/api.service';
 import BasicList from '@/components/ui/BasicList2.vue';
+import { reactive, watch } from 'vue';
+import { useGroup } from '@/components/groups/group';
+import { useRoute } from 'vue-router';
+import store from '@/store';
 
-export default {
-  components: {
-    BasicList,
-  },
-  data() {
-    return {
-      entities: [],
-      showArchived: false,
-      loading: false,
-    };
-  },
-  methods: {
-    async fetchEntities() {
-      try {
-        this.loading = true;
-        if (this.isWhitelabel) {
-          const { path } = this.whitelabelPartner;
-          const { data: entities } = await api.get(`/groups/all?showArchived=${this.showArchived}&prefix=${path}`);
-          this.entities = entities;
-          return;
-        }
+const route = useRoute();
+const { isWhitelabel, getWhitelabelPartner } = useGroup();
 
-        if (this.onlyMyGroups) {
-          this.entities = this.$store.getters['memberships/groups'];
-        } else {
-          const { data: entities } = await api.get(`/groups/all?showArchived=${this.showArchived}`);
-          this.entities = entities;
-        }
-      } finally {
-        this.loading = false;
-      }
-    },
-  },
-  computed: {
-    isWhitelabel() {
-      return this.$store.getters['whitelabel/isWhitelabel'];
-    },
-    whitelabelPartner() {
-      return this.$store.getters['whitelabel/partner'];
-    },
-    rootDir() {
-      if (this.isWhitelabel) {
-        return this.whitelabelPartner.path;
-      }
-      return '/';
-    },
-    onlyMyGroups() {
-      return this.$route.matched.some(({ name }) => name === 'my-groups-list');
-    },
-  },
-  watch: {
-    showArchived() {
-      this.fetchEntities();
-    },
-  },
-  created() {
-    this.fetchEntities();
-  },
-};
+const state = reactive({
+  entities: [],
+  showArchived: false,
+  loading: false,
+});
+
+fetchEntities();
+
+async function fetchEntities() {
+  try {
+    state.loading = true;
+    if (isWhitelabel()) {
+      const { path } = getWhitelabelPartner();
+      const { data: entities } = await api.get(`/groups/all?showArchived=${state.showArchived}&prefix=${path}`);
+      state.entities = entities;
+      return;
+    }
+
+    if (onlyMyGroups()) {
+      state.entities = store.getters['memberships/groups'];
+    } else {
+      const { data: entities } = await api.get(`/groups/all?showArchived=${state.showArchived}`);
+      state.entities = entities;
+    }
+  } finally {
+    state.loading = false;
+  }
+}
+
+function onlyMyGroups() {
+  return route.matched.some(({ name }) => name === 'my-groups-list');
+}
+
+function rootDir() {
+  if (isWhitelabel()) {
+    return getWhitelabelPartner().path;
+  }
+  return '/';
+}
+
+watch(
+  () => state.showArchived,
+  async () => {
+    await fetchEntities();
+  }
+);
 </script>
