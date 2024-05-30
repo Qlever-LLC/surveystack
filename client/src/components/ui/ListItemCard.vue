2c7c2c802b6f9f0c146a0c9e283e1688960500eb
Benoit De Sousa <benoit.desousa@nexus-computing.ch>
2024-05-30 20:25:10 +0200

    adapt render of listItemCard

--- client/src/components/ui/ListItemCard.vue
+++ client/src/components/ui/ListItemCard.vue
@@ -26,7 +26,7 @@
           <span class="entityName_deepCSS">
             <span class=""> {{ state.entity.name }}</span>
             <br />
-            <span class="subEntityName_deepCSS">{{ state.entity?.role }}</span>
+            <span v-if="!smallCard" class="subEntityName_deepCSS">{{ state.entity?.role }}</span>
           </span>
           <span v-if="questionSetsType">
             <a-icon class="ml-2 my-2">mdi-note-multiple-outline</a-icon>
