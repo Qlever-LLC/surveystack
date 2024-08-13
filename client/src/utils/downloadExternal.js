cb8d52b45d00cc12039cd927675f6e7b161c103d
Benoit De Sousa <benoit.desousa@nexus-computing.ch>
2024-08-13 10:36:33 +0200

    Fix problem with external download of survey CSV and JSON files

--- client/src/utils/downloadExternal.js
+++ client/src/utils/downloadExternal.js
@@ -1,11 +1,24 @@
-function downloadExternal(href, download) {
-  const element = document.createElement('a');
-  element.setAttribute('href', href);
-  element.setAttribute('download', download);
-  element.style.display = 'none';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+async function downloadExternal(href, download) {
+  try {
+    const response = await fetch(href);
+
+    if (!response.ok) {
+      throw new Error('Network response error');
+    }
+
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const element = document.createElement('a');
+    element.href = url;
+    element.setAttribute('download', download);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+    window.URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error('An error occurred while trying to download the file.', error);
+  }
 }
 
 export default downloadExternal;
