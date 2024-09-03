abf7c5acaa085f7e09bac4dc5ef625f5e8e6e564
Benoit De Sousa <benoit.desousa@nexus-computing.ch>
2024-09-03 19:15:55 +0200

    fix unit test
    Co-authored-by: cryinbockritz <cryinbockritz@users.noreply.github.com>

--- client/src/components/ui/Register.spec.js
+++ client/src/components/ui/Register.spec.js
@@ -3,25 +3,29 @@ import { renderWithVuetify } from '../../../tests/renderWithVuetify';
 import Register from './Register.vue';
 import mockAxios from 'axios';
 import { flushPromises } from '@vue/test-utils';
+import { createStore } from 'vuex';
+import { createStoreObject } from '@/store';
+import { createAppRouter } from '@/router';
+
+const renderRegister = ({ props } = {}, store, router) =>
+  renderWithVuetify(Register, { props: { ...props } }, store, router);
 
 describe('Register component', () => {
   describe('navigation links and buttons', () => {
     it('Renders link to Register by default', async () => {
-      const { getByRole } = renderWithVuetify(Register);
+      const { getByRole } = renderRegister();
       getByRole('link', { name: 'Already have an account?' });
     });
 
     it('Renders button to Register when useLink is false', async () => {
-      const { getByRole } = renderWithVuetify(Register, {
-        props: { useLink: false },
-      });
+      const { getByRole } = renderRegister({ props: { useLink: false } });
       getByRole('button', { name: 'Already have an account?' });
     });
   });
 
   describe('submitting form and check submit() method behaviour based on user fields', () => {
     it('shows an error when password is passed without email', async () => {
-      const { getByLabelText, getByText, queryByText } = renderWithVuetify(Register);
+      const { getByLabelText, getByText, queryByText } = renderRegister();
       const passwordInput = getByLabelText('Password');
       fireEvent.update(passwordInput, 'somePassword');
       expect(passwordInput.value).toBe('somePassword');
@@ -32,7 +36,7 @@ describe('Register component', () => {
     });
 
     it('shows an error when email is passed without password', async () => {
-      const { getByLabelText, getByText, queryByText } = renderWithVuetify(Register);
+      const { getByLabelText, getByText, queryByText } = renderRegister();
       const emailInput = getByLabelText('E-Mail');
       fireEvent.update(emailInput, 'email.error@error.com');
       expect(emailInput.value).toBe('email.error@error.com');
@@ -49,40 +53,32 @@ describe('Register component', () => {
       let res = { data: { meta: { invitationOnly: false } } };
       mockAxios.get.mockImplementation(() => Promise.resolve(res));
       mockAxios.post.mockImplementation(() => Promise.resolve({ data: 'dummy data' }));
-      const push = jest.fn();
-      const { getByLabelText, getByText } = renderWithVuetify(Register, {
-        global: {
-          mocks: {
-            $store: {
-              getters: {
-                'whitelabel/isWhitelabel': true,
-                'whitelabel/partner': {
-                  id: WHITELABEL_GROUP_ID,
-                },
-                'auth/user': {
-                  _id: 2,
-                },
-                'auth/isLoggedIn': true,
+      const store = createStore(createStoreObject());
+      const dispatchMock = jest.fn((action) => {
+        if (action === 'memberships/getUserMemberships') {
+          return Promise.resolve([
+            {
+              group: {
+                _id: WHITELABEL_GROUP_ID,
               },
-              dispatch: jest.fn((action) => {
-                if (action === 'memberships/getUserMemberships') {
-                  return Promise.resolve([
-                    {
-                      group: {
-                        _id: WHITELABEL_GROUP_ID,
-                      },
-                    },
-                  ]);
-                }
-              }),
             },
-            $route: {
-              query: { redirect: false },
-            },
-            $router: { push },
-          },
-        },
+          ]);
+        }
       });
+      store.dispatch = dispatchMock;
+      const router = createAppRouter();
+      jest.spyOn(router, 'push');
+      store.getters = {
+        'whitelabel/isWhitelabel': true,
+        'whitelabel/partner': {
+          id: WHITELABEL_GROUP_ID,
+        },
+        'auth/user': {
+          _id: 2,
+        },
+        'auth/isLoggedIn': true,
+      };
+      const { getByLabelText, getByText } = renderRegister({}, store, router);
       const emailInput = getByLabelText('E-Mail');
       fireEvent.update(emailInput, 'email@mail.com');
       const passwordInput = getByLabelText('Password');
@@ -93,7 +89,7 @@ describe('Register component', () => {
       await flushPromises();
 
       expect(mockAxios.post).toHaveBeenCalledTimes(1);
-      expect(push).toHaveBeenCalledWith(`/groups/${WHITELABEL_GROUP_ID}`);
+      expect(router.push).toHaveBeenCalledWith(`/groups/${WHITELABEL_GROUP_ID}`);
     });
 
     it('submit if this.isWhitelabel, but autojoin throws an error', async () => {
@@ -104,34 +100,26 @@ describe('Register component', () => {
           response: { data: { message: 'an error message' } },
         })
       );
-      const push = jest.fn();
-      const { getByLabelText, getByText } = renderWithVuetify(Register, {
-        global: {
-          mocks: {
-            $store: {
-              getters: {
-                'whitelabel/isWhitelabel': true,
-                'whitelabel/partner': {
-                  id: WHITELABEL_GROUP_ID,
-                },
-                'auth/user': {
-                  _id: 2,
-                },
-                'auth/isLoggedIn': true,
-              },
-              dispatch: jest.fn((action) => {
-                if (action === 'memberships/getUserMemberships') {
-                  return Promise.resolve([]);
-                }
-              }),
-            },
-            $route: {
-              query: { redirect: false },
-            },
-            $router: { push },
-          },
-        },
+      const store = createStore(createStoreObject());
+      const dispatchMock = jest.fn((action) => {
+        if (action === 'memberships/getUserMemberships') {
+          return Promise.resolve([]);
+        }
       });
+      store.dispatch = dispatchMock;
+      const router = createAppRouter();
+      jest.spyOn(router, 'push');
+      store.getters = {
+        'whitelabel/isWhitelabel': true,
+        'whitelabel/partner': {
+          id: WHITELABEL_GROUP_ID,
+        },
+        'auth/user': {
+          _id: 2,
+        },
+        'auth/isLoggedIn': true,
+      };
+      const { getByLabelText, getByText } = renderRegister({}, store, router);
       const emailInput = getByLabelText('E-Mail');
       fireEvent.update(emailInput, 'email@mail.com');
       const passwordInput = getByLabelText('Password');
@@ -142,7 +130,7 @@ describe('Register component', () => {
       await flushPromises();
 
       expect(mockAxios.post).toHaveBeenCalledTimes(1);
-      expect(push).toHaveBeenCalledWith('/'); //if user could not join the whitelabel group, route to /
+      expect(router.push).toHaveBeenCalledWith('/'); //if user could not join the whitelabel group, route to /
     });
   });
 
@@ -163,27 +151,18 @@ describe('Register component', () => {
     });
 
     it("submit and get the redirection 'this.$route.params.redirect = 'true'' ", async () => {
-      const push = jest.fn();
-      const { getByLabelText, getByText } = renderWithVuetify(Register, {
-        global: {
-          mocks: {
-            $store: {
-              getters: {
-                'whitelabel/isWhitelabel': false,
-                'auth/user': {
-                  _id: 2,
-                },
-                'auth/isLoggedIn': true,
-              },
-              dispatch: mockDispatchFn,
-            },
-            $route: {
-              query: { redirect: true },
-            },
-            $router: { push },
-          },
+      const store = createStore(createStoreObject());
+      store.dispatch = mockDispatchFn;
+      const router = createAppRouter();
+      jest.spyOn(router, 'push');
+      store.getters = {
+        'whitelabel/isWhitelabel': false,
+        'auth/user': {
+          _id: 2,
         },
-      });
+        'auth/isLoggedIn': true,
+      };
+      const { getByLabelText, getByText } = renderRegister({ props: { redirect: true } }, store, router);
       const emailInput = getByLabelText('E-Mail');
       fireEvent.update(emailInput, 'email@mail.com');
       const passwordInput = getByLabelText('Password');
@@ -193,31 +172,22 @@ describe('Register component', () => {
       await fireEvent.click(button);
       await flushPromises();
 
-      expect(push).toHaveBeenCalledWith(true);
+      expect(router.push).toHaveBeenCalledWith(true);
     });
 
     it("submit and get the redirection '/groups/2' ", async () => {
-      const push = jest.fn();
-      const { getByLabelText, getByText } = renderWithVuetify(Register, {
-        global: {
-          mocks: {
-            $store: {
-              getters: {
-                'whitelabel/isWhitelabel': false,
-                'auth/user': {
-                  _id: 2,
-                },
-                'auth/isLoggedIn': true,
-              },
-              dispatch: mockDispatchFn,
-            },
-            $route: {
-              query: { redirect: false },
-            },
-            $router: { push },
-          },
+      const store = createStore(createStoreObject());
+      store.dispatch = mockDispatchFn;
+      const router = createAppRouter();
+      jest.spyOn(router, 'push');
+      store.getters = {
+        'whitelabel/isWhitelabel': false,
+        'auth/user': {
+          _id: 2,
         },
-      });
+        'auth/isLoggedIn': true,
+      };
+      const { getByLabelText, getByText } = renderRegister({ props: { redirect: false } }, store, router);
       const emailInput = getByLabelText('E-Mail');
       fireEvent.update(emailInput, 'email@mail.com');
       const passwordInput = getByLabelText('Password');
@@ -227,30 +197,27 @@ describe('Register component', () => {
       await fireEvent.click(button);
       await flushPromises();
 
-      expect(push).toHaveBeenCalledWith('/groups/2');
+      expect(router.push).toHaveBeenCalledWith('/groups/2');
     });
   });
 
   describe('submit form and check submit() method behaviour based on throw error', () => {
     it('displays an error when status 409', async () => {
-      const { getByLabelText, getByText, findByText } = renderWithVuetify(Register, {
-        global: {
-          mocks: {
-            $store: {
-              getters: {
-                'whitelabel/isWhitelabel': false,
-              },
-              dispatch: (action) => {
-                if (action === 'auth/login') {
-                  return Promise.reject({
-                    response: { status: 409, data: { message: 'error message' } },
-                  });
-                }
-              },
-            },
-          },
-        },
+      const mockDispatchFn = jest.fn((action) => {
+        if (action === 'auth/login') {
+          return Promise.reject({
+            response: { status: 409, data: { message: 'error message' } },
+          });
+        }
       });
+      const store = createStore(createStoreObject());
+      store.dispatch = mockDispatchFn;
+      const router = createAppRouter();
+      jest.spyOn(router, 'push');
+      store.getters = {
+        'whitelabel/isWhitelabel': false,
+      };
+      const { getByLabelText, getByText, findByText } = renderRegister({ props: { redirect: false } }, store, router);
       const emailInput = getByLabelText('E-Mail');
       fireEvent.update(emailInput, 'email@mail.com');
       const passwordInput = getByLabelText('Password');
@@ -262,24 +229,21 @@ describe('Register component', () => {
     });
 
     it('displays an error when status 400', async () => {
-      const { getByLabelText, getByText, findByText } = renderWithVuetify(Register, {
-        global: {
-          mocks: {
-            $store: {
-              getters: {
-                'whitelabel/isWhitelabel': false,
-              },
-              dispatch: (action) => {
-                if (action === 'auth/login') {
-                  return Promise.reject({
-                    response: { status: 400, data: { message: 'error message' } },
-                  });
-                }
-              },
-            },
-          },
-        },
+      const mockDispatchFn = jest.fn((action) => {
+        if (action === 'auth/login') {
+          return Promise.reject({
+            response: { status: 400, data: { message: 'error message' } },
+          });
+        }
       });
+      const store = createStore(createStoreObject());
+      store.dispatch = mockDispatchFn;
+      const router = createAppRouter();
+      jest.spyOn(router, 'push');
+      store.getters = {
+        'whitelabel/isWhitelabel': false,
+      };
+      const { getByLabelText, getByText, findByText } = renderRegister({ props: { redirect: false } }, store, router);
       const emailInput = getByLabelText('E-Mail');
       fireEvent.update(emailInput, 'email@mail.com');
       const passwordInput = getByLabelText('Password');
@@ -291,24 +255,21 @@ describe('Register component', () => {
     });
 
     it('displays an error when an error occurred and status is different to 409 and 400 (ex: status 500)', async () => {
-      const { getByLabelText, getByText, findByText } = renderWithVuetify(Register, {
-        global: {
-          mocks: {
-            $store: {
-              getters: {
-                'whitelabel/isWhitelabel': false,
-              },
-              dispatch: (action) => {
-                if (action === 'auth/login') {
-                  return Promise.reject({
-                    response: { status: 500, data: { message: 'Unknown error :/' } },
-                  });
-                }
-              },
-            },
-          },
-        },
+      const mockDispatchFn = jest.fn((action) => {
+        if (action === 'auth/login') {
+          return Promise.reject({
+            response: { status: 500, data: { message: 'Unknown error :/' } },
+          });
+        }
       });
+      const store = createStore(createStoreObject());
+      store.dispatch = mockDispatchFn;
+      const router = createAppRouter();
+      jest.spyOn(router, 'push');
+      store.getters = {
+        'whitelabel/isWhitelabel': false,
+      };
+      const { getByLabelText, getByText, findByText } = renderRegister({ props: { redirect: false } }, store, router);
       const emailInput = getByLabelText('E-Mail');
       fireEvent.update(emailInput, 'email@mail.com');
       const passwordInput = getByLabelText('Password');
