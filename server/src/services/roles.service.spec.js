3dadf599d216497d34ef3830190ee84aa6fd62c1
Brian Kockritz <8039912-cryinbockritz@users.noreply.gitlab.com>
2025-01-07 11:19:30 -0500

    roles.service.spec.js: remove lines from test that have no effect

--- server/src/services/roles.service.spec.js
+++ server/src/services/roles.service.spec.js
@@ -11,27 +11,23 @@ describe('rolesService', () => {
     await groupCommunity.createSubGroup({ name: 'Lab' });
 
     const a = await getGroupAndDescendants(groupBionutrient);
-    expect(a._id).not.toBeNaN();
-    expect.objectContaining(a[0].meta);
+    expect(a[0]._id).toBeDefined();
     expect(a[0].name).toBe('Bionutrient');
     expect(a[0].slug).toBe('bionutrient');
     expect(a[0].dir).toBe('/');
     expect(a[0].path).toBe('/bionutrient/');
-    expect.objectContaining(a[0].surveys);
     expect(a).toHaveLength(6);
 
     const b = await getGroupAndDescendants(groupBionutrient, {});
-    expect(b._id).not.toBeNaN();
-    expect.objectContaining(b[0].meta);
+    expect(b[0]._id).toBeDefined();
     expect(b[0].name).toBe('Bionutrient');
     expect(b[0].slug).toBe('bionutrient');
     expect(b[0].dir).toBe('/');
     expect(b[0].path).toBe('/bionutrient/');
-    expect.objectContaining(b[0].surveys);
     expect(b).toHaveLength(6);
 
     const c = await getGroupAndDescendants(groupBionutrient, { name: 1, dir: 1 });
-    expect(c._id).not.toBeNaN();
+    expect(c[0]._id).toBeDefined();
     expect(c[0].meta).toBeUndefined();
     expect(c[0].name).toBe('Bionutrient');
     expect(c[0].slug).toBeUndefined();
